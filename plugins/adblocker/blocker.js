b0e3615fc90783a23513356aa1893a83e655c192
TC <th-ch@users.noreply.github.com>
2020-04-11 14:00:12 +0200

    Bump electron to last version

--- plugins/adblocker/blocker.js
+++ plugins/adblocker/blocker.js
@@ -1,11 +1,8 @@
 const { containsAds } = require("./contains-ads");
 
 module.exports.blockWindowAds = webContents => {
-	webContents.session.webRequest.onBeforeRequest(
-		["*://*./*"],
-		(details, cb) => {
-			const shouldBeBlocked = containsAds(details.url);
-			cb({ cancel: shouldBeBlocked });
-		}
-	);
+	webContents.session.webRequest.onBeforeRequest((details, cb) => {
+		const shouldBeBlocked = containsAds(details.url);
+		cb({ cancel: shouldBeBlocked });
+	});
 };
