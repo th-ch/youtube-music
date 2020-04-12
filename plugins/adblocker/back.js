422c3fc28d83da309a80447dcd5064a4346580e8
TC <th-ch@users.noreply.github.com>
2020-04-12 13:23:35 +0200

    Migrate from adblock-rs to cliqz

--- plugins/adblocker/back.js
+++ plugins/adblocker/back.js
@@ -1,3 +1,12 @@
-const { blockWindowAds } = require("./blocker");
+const { ElectronBlocker } = require("@cliqz/adblocker-electron");
+const { session } = require("electron");
+const fetch = require("node-fetch");
 
-module.exports = win => blockWindowAds(win.webContents);
+const SOURCES = [
+	"https://raw.githubusercontent.com/kbinani/adblock-youtube-ads/master/signed.txt"
+];
+
+module.exports = () =>
+	ElectronBlocker.fromLists(fetch, SOURCES)
+		.then(blocker => blocker.enableBlockingInSession(session.defaultSession))
+		.catch(err => console.log("Error loading adBlocker", err));
--- plugins/adblocker/blocker.js
+++
@@ -1,8 +0,0 @@
-const { containsAds } = require("./contains-ads");
-
-module.exports.blockWindowAds = webContents => {
-	webContents.session.webRequest.onBeforeRequest((details, cb) => {
-		const shouldBeBlocked = containsAds(details.url);
-		cb({ cancel: shouldBeBlocked });
-	});
-};
