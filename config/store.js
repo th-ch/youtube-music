a5b23d925d02fdaefe023a22e8d83fdaa88485b1
TC <th-ch@users.noreply.github.com>
2020-12-03 18:25:08 +0100

    Move migrations into separate const

--- config/store.js
+++ config/store.js
@@ -3,24 +3,26 @@ const Store = require("electron-store");
 
 const defaults = require("./defaults");
 
+const migrations = {
+	">=1.7.0": (store) => {
+		const enabledPlugins = store.get("plugins");
+		if (!Array.isArray(enabledPlugins)) {
+			console.warn("Plugins are not in array format, cannot migrate");
+			return;
+		}
+
+		const plugins = {};
+		enabledPlugins.forEach((enabledPlugin) => {
+			plugins[enabledPlugin] = {
+				enabled: true,
+			};
+		});
+		store.set("plugins", plugins);
+	},
+};
+
 module.exports = new Store({
 	defaults,
 	clearInvalidConfig: false,
-	migrations: {
-		">=1.7.0": (store) => {
-			const enabledPlugins = store.get("plugins");
-			if (!Array.isArray(enabledPlugins)) {
-				console.warn("Plugins are not in array format, cannot migrate");
-				return;
-			}
-
-			const plugins = {};
-			enabledPlugins.forEach((enabledPlugin) => {
-				plugins[enabledPlugin] = {
-					enabled: true,
-				};
-			});
-			store.set("plugins", plugins);
-		},
-	},
+	migrations,
 });
