f71e0e9da99db3d5fe8068c8d3cee281a55d3982
Sem Vissscher <semvi@Macbook-pro-van-Sem.local>
2020-12-16 20:30:01 +0100

    Switched like/dislike positions
    Switched like/dislike positions to match youtube musics layout

--- plugins/touchbar/back.js
+++ plugins/touchbar/back.js
@@ -17,7 +17,7 @@ const titleSelector = '.title.style-scope.ytmusic-player-bar';
 const imageSelector = '#layout > ytmusic-player-bar > div.middle-controls.style-scope.ytmusic-player-bar > img';
 
 // These keys will be used to go backwards, pause, skip songs, like songs, dislike songs
-const keys = ['k', 'space', 'j', '+', '_'];
+const keys = ['k', 'space', 'j', '_', '+'];
 
 const presskey = (window, key) => {
 	window.webContents.sendInputEvent({
@@ -67,10 +67,10 @@ module.exports = win => {
 				label: '⏭'
 			}),
 			new TouchBarButton({
-				label: '👍'
+				label: '👎'
 			}),
 			new TouchBarButton({
-				label: '👎'
+				label: '👍'
 			})
 		],
 		change: i => presskey(win, keys[i])
