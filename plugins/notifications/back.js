d945e0d0cbf9c15ab9483c132a18211b3b6d2568
Sem Vissscher <semvi@Macbook-pro-van-Sem.local>
2020-12-21 21:35:02 +0100

    Simplifies the notification plugin to use the globalized song info

--- plugins/notifications/back.js
+++ plugins/notifications/back.js
@@ -1,33 +1,31 @@
-const { nativeImage, Notification } = require("electron");
-
-const { listenAction } = require("../utils");
-const { ACTIONS, CHANNEL } = require("./actions.js");
+const {Notification} = require('electron');
 
 function notify(info) {
-	let notificationImage = "assets/youtube-music.png";
+	let notificationImage = 'assets/youtube-music.png';
+
 	if (info.image) {
-		notificationImage = nativeImage.createFromDataURL(info.image);
+		notificationImage = info.image.resize({height: 256, width: 256});
 	}
 
+	// Fill the notification with content
 	const notification = {
-		title: info.title || "Playing",
+		title: info.title || 'Playing',
 		body: info.artist,
 		icon: notificationImage,
-		silent: true,
+		silent: true
 	};
+	// Send the notification
 	new Notification(notification).show();
 }
 
-function listenAndNotify() {
-	listenAction(CHANNEL, (event, action, imageSrc) => {
-		switch (action) {
-			case ACTIONS.NOTIFICATION:
-				notify(imageSrc);
-				break;
-			default:
-				console.log("Unknown action: " + action);
-		}
+module.exports = win => {
+	win.on('ready-to-show', () => {
+		// Register the callback for new song information
+		global.songInfo.onNewData(songInfo => {
+			// If song is playing send notification
+			if (!songInfo.isPaused) {
+				notify(songInfo);
+			}
+		});
 	});
-}
-
-module.exports = listenAndNotify;
+};
