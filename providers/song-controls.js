aaff4a2d7d6375fa12f277db05f6e566fc41160e
TC <th-ch@users.noreply.github.com>
2021-01-15 22:31:46 +0100

    Add shortcuts to provider

--- providers/song-controls.js
+++ providers/song-controls.js
@@ -1,18 +1,40 @@
 // This is used for to control the songs
-const pressKey = (window, key) => {
+const pressKey = (window, key, modifiers = []) => {
 	window.webContents.sendInputEvent({
 		type: "keydown",
+		modifiers,
 		keyCode: key,
 	});
 };
 
 module.exports = (win) => {
 	return {
+		// Playback
 		previous: () => pressKey(win, "k"),
 		next: () => pressKey(win, "j"),
 		playPause: () => pressKey(win, "space"),
 		like: () => pressKey(win, "_"),
 		dislike: () => pressKey(win, "+"),
+		go10sBack: () => pressKey(win, "h"),
+		go10sForward: () => pressKey(win, "l"),
+		go1sBack: () => pressKey(win, "h", ["shift"]),
+		go1sForward: () => pressKey(win, "l", ["shift"]),
+		shuffle: () => pressKey(win, "s"),
+		switchRepeat: () => pressKey(win, "r"),
+		// General
+		volumeMinus10: () => pressKey(win, "-"),
+		volumePlus10: () => pressKey(win, "="),
+		dislikeAndNext: () => pressKey(win, "-", ["shift"]),
+		like: () => pressKey(win, "=", ["shift"]),
+		fullscreen: () => pressKey(win, "f"),
+		muteUnmute: () => pressKey(win, "m"),
+		maximizeMinimisePlayer: () => pressKey(win, "q"),
+		// Navigation
+		goToHome: () => pressKey(win, "h", ["g"]),
+		goToLibrary: () => pressKey(win, "l", ["g"]),
+		goToHotlist: () => pressKey(win, "t", ["g"]),
+		goToSettings: () => pressKey(win, ",", ["g"]),
 		search: () => pressKey(win, "/"),
+		showShortcuts: () => pressKey(win, "/", ["shift"]),
 	};
 };
