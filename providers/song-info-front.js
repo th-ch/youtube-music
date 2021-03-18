aa80b5d1065d2e174faa2943650259f7cbe4ba81
Sem Visscher <semvisscher10@gmail.com>
2021-03-18 17:53:30 +0100

    small readability changes

--- providers/song-info-front.js
+++ providers/song-info-front.js
@@ -4,18 +4,19 @@ const injectListener = () => {
 	var oldXHR = window.XMLHttpRequest;
 	function newXHR() {
 		var realXHR = new oldXHR();
-		realXHR.addEventListener("readystatechange", function() {
+		realXHR.addEventListener("readystatechange", () => {
 			if(realXHR.readyState==4 && realXHR.status==200){
-				if (realXHR.responseURL.includes('/player'))
+				if (realXHR.responseURL.includes('/player')){
 					// if the request is the contains the song info send the response to ipcMain
 					ipcRenderer.send(
 						"song-info-request",
 						realXHR.responseText
 					);
+				}
 			}
 		}, false);
 		return realXHR;
 	}
 	window.XMLHttpRequest = newXHR;
 }
-module.exports = injectListener
+module.exports = injectListener;
