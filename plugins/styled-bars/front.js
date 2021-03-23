e53a1f1edeb7a51c9a41f8596f37ff779b5e1b9c
Araxeus <78568641+Araxeus@users.noreply.github.com>
2021-03-23 03:58:19 +0200

    xo lint --fix

--- plugins/styled-bars/front.js
+++ plugins/styled-bars/front.js
@@ -1,10 +1,10 @@
 const customTitlebar = require('custom-electron-titlebar');
 
 module.exports = () => {
-    const myBar = new customTitlebar.Titlebar({
-        backgroundColor: customTitlebar.Color.fromHex('#030303'),
-        itemBackgroundColor: customTitlebar.Color.fromHex('#121212'), //#020
-    });
-    myBar.updateTitle(' ');
-    document.title = "Youtube Music";
-}
+	const myBar = new customTitlebar.Titlebar({
+		backgroundColor: customTitlebar.Color.fromHex('#030303'),
+		itemBackgroundColor: customTitlebar.Color.fromHex('#121212') // #020
+	});
+	myBar.updateTitle(' ');
+	document.title = 'Youtube Music';
+};
