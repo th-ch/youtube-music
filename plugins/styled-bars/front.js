f1dbe4ab6c9c65331f8d7b30a5c350dbc11cad2f
Araxeus <78568641+Araxeus@users.noreply.github.com>
2021-03-23 15:07:31 +0200

    Allow hide menu options (uses Escape key)

--- plugins/styled-bars/front.js
+++ plugins/styled-bars/front.js
@@ -1,10 +1,21 @@
 const customTitlebar = require('custom-electron-titlebar');
+const {remote, ipcRenderer} = require('electron');
 
 module.exports = () => {
+	//const windowProxy = window.open('https://music.youtube.com', null);
 	const myBar = new customTitlebar.Titlebar({
 		backgroundColor: customTitlebar.Color.fromHex('#030303'),
-		itemBackgroundColor: customTitlebar.Color.fromHex('#121212') // #020
+		itemBackgroundColor: customTitlebar.Color.fromHex('#121212')
 	});
 	myBar.updateTitle(' ');
 	document.title = 'Youtube Music';
+
+	ipcRenderer.on('updateMenu', function(event, menu) {
+		//let menu = Menu.buildFromTemplate(template);
+		//Menu.setApplicationMenu(menu);
+		if(menu)
+			myBar.updateMenu(remote.Menu.getApplicationMenu());
+		else
+			myBar.updateMenu(null);
+	});
 };
