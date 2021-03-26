0aed38ef1ed5dd177d2b94e51a10b152c03edd23
Araxeus <78568641+Araxeus@users.noreply.github.com>
2021-03-26 17:52:23 +0300

    fix array input + use getFolder()

--- plugins/downloader/menu.js
+++ plugins/downloader/menu.js
@@ -67,11 +67,11 @@ module.exports = (win, options, refreshMenu) => [
 		click: () => {
 			let result = dialog.showOpenDialogSync({ 
 				properties: ['openDirectory', 'createDirectory'],
-				defaultPath: Array.isArray(options.downloadFolder) ? options.downloadFolder[0] : undefined,
+				defaultPath: getFolder(options.downloadFolder),
 			})
 			if(result) {
-				options.downloadFolder = result
-				setOptions("downloader", options)
+				options.downloadFolder = result[0];
+				setOptions("downloader", options);
 			} //else = user pressed cancel
 		}
 	},
