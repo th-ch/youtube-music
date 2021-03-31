0d2b61472c1e9bc3f6511eb35ec742c8f5571be1
TC <th-ch@users.noreply.github.com>
2021-03-31 20:25:27 +0200

    Use template builder in custom menu plugin

--- plugins/styled-bars/back.js
+++ plugins/styled-bars/back.js
@@ -5,24 +5,10 @@ const electronLocalshortcut = require("electron-localshortcut");
 const config = require("../../config");
 var { mainMenuTemplate } = require("../../menu");
 
-//override menu template for custom menu
-const originTemplate = mainMenuTemplate;
-mainMenuTemplate = function (winHook) {
-	//get template
-	let template = originTemplate(winHook, false, (item) => {
-		checkCheckbox(item);
-	});
-	//return as normal
-	return template;
-};
-//win hook for fixing menu
-let win;
-
 //check that menu doesn't get created twice
 let done = false;
 
-module.exports = (winImport) => {
-	win = winImport;
+module.exports = (win) => {
 	// css for custom scrollbar + disable drag area(was causing bugs)
 	injectCSS(win.webContents, path.join(__dirname, "style.css"));
 	win.on("ready-to-show", () => {
@@ -31,27 +17,29 @@ module.exports = (winImport) => {
 			return;
 		}
 		done = true;
-		let template = mainMenuTemplate(win);
+		let template = mainMenuTemplate(win, false, (item) => {
+			checkCheckbox(win, item);
+		});
 		let menu = Menu.buildFromTemplate(template);
 		Menu.setApplicationMenu(menu);
 
 		//register keyboard shortcut && hide menu if hideMenu is enabled
 		if (config.get("options.hideMenu")) {
-			switchMenuVisibility();
+			switchMenuVisibility(win);
 			electronLocalshortcut.register(win, "Esc", () => {
-				switchMenuVisibility();
+				switchMenuVisibility(win);
 			});
 		}
 	});
 };
 
 let visible = true;
-function switchMenuVisibility() {
+function switchMenuVisibility(win) {
 	visible = !visible;
 	win.webContents.send("updateMenu", visible);
 }
 
-function checkCheckbox(item) {
+function checkCheckbox(win, item) {
 	//check item
 	item.checked = !item.checked;
 	//update menu (closes it)
