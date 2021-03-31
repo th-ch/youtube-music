a8cfe399b3a5b1b9ecee5304792925e7203f2ccc
TC <th-ch@users.noreply.github.com>
2021-03-31 21:15:52 +0200

    Rename bar var

--- plugins/styled-bars/front.js
+++ plugins/styled-bars/front.js
@@ -3,19 +3,19 @@ const { remote, ipcRenderer } = require("electron");
 const customTitlebar = require("custom-electron-titlebar");
 
 module.exports = () => {
-	const myBar = new customTitlebar.Titlebar({
+	const bar = new customTitlebar.Titlebar({
 		backgroundColor: customTitlebar.Color.fromHex("#050505"),
 		itemBackgroundColor: customTitlebar.Color.fromHex("#121212"),
 	});
-	myBar.updateTitle(" ");
+	bar.updateTitle(" ");
 	document.title = "Youtube Music";
 
 	ipcRenderer.on("updateMenu", function (event, menu) {
 		if (menu) {
-			myBar.updateMenu(remote.Menu.getApplicationMenu());
+			bar.updateMenu(remote.Menu.getApplicationMenu());
 		} else {
 			try {
-				myBar.updateMenu(null);
+				bar.updateMenu(null);
 			} catch (e) {
 				//will always throw type error - null isn't menu, but it works
 			}
