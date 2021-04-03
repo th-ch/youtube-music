b5c60ee6a90311a1409331044076b89e1616942a
TC <th-ch@users.noreply.github.com>
2021-04-03 11:35:31 +0200

    Re-format front metadata lib

--- providers/song-info-front.js
+++ providers/song-info-front.js
@@ -13,19 +13,20 @@ const injectListener = () => {
 	var oldXHR = window.XMLHttpRequest;
 	function newXHR() {
 		var realXHR = new oldXHR();
-		realXHR.addEventListener("readystatechange", () => {
-			if(realXHR.readyState==4 && realXHR.status==200){
-				if (realXHR.responseURL.includes('/player')){
-					// if the request is the contains the song info send the response to ipcMain
-					ipcRenderer.send(
-						"song-info-request",
-						realXHR.responseText
-					);
+		realXHR.addEventListener(
+			"readystatechange",
+			() => {
+				if (realXHR.readyState == 4 && realXHR.status == 200) {
+					if (realXHR.responseURL.includes("/player")) {
+						// if the request contains the song info, send the response to ipcMain
+						ipcRenderer.send("song-info-request", realXHR.responseText);
+					}
 				}
-			}
-		}, false);
+			},
+			false
+		);
 		return realXHR;
 	}
 	window.XMLHttpRequest = newXHR;
-}
+};
 module.exports = injectListener;
