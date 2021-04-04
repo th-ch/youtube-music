73b0ddc2cecb08509a17bef4d222e8f5b183599e
Araxeus <78568641+Araxeus@users.noreply.github.com>
2021-04-04 21:25:41 +0300

    css tweaks

--- prompt/customTitlebar.js
+++ prompt/customTitlebar.js
@@ -3,10 +3,17 @@ const customTitlebar = require("custom-electron-titlebar");
 module.exports = () => {
     const bar = new customTitlebar.Titlebar({
 		backgroundColor: customTitlebar.Color.fromHex("#050505"),
+        minimizable: false,
+        maximizable: false,
+        unfocusEffect: true,
 	});
     try {
         bar.updateMenu(null);
     } catch (e) {
         //will always throw type error - null isn't menu, but it works
     }
+    let container = document.querySelector('#container');
+    container.style.width = '100%';
+    container.style.position = 'fixed';
+    container.style.border = 'unset';
 }
