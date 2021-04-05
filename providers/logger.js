11bd1adbd406160b9ee4215fa0b329b5ecd4391d
Araxeus <78568641+Araxeus@users.noreply.github.com>
2021-04-05 03:25:45 +0300

    stylecheck

--- providers/logger.js
+++ providers/logger.js
@@ -2,13 +2,13 @@ const { ipcRenderer } = require("electron");
 
 module.exports = () => {
     ipcRenderer.on("log", (event, log) => {
-		let string = log.toString() || log;
-		if (string) {
-		console.log(string);
-		} else {
-			for (let propery of log) {
-			console.log(propery.toString() || propery);
-			}
-		}
-	})
+        let string = log.toString() || log;
+        if (string) {
+            console.log(string);
+        } else {
+            for (let propery of log) {
+                console.log(propery.toString() || propery);
+            }
+        }
+    })
 }
