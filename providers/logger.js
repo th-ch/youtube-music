106e461beb5a7cc4a92f8970acd041cd1b09b8f9
Araxeus <78568641+Araxeus@users.noreply.github.com>
2021-04-05 04:08:55 +0300

    fix typo

--- providers/logger.js
+++ providers/logger.js
@@ -2,7 +2,7 @@ const { ipcRenderer } = require("electron");
 
 module.exports = () => {
     ipcRenderer.on("log", (event, log) => {
-        let string = log.toString() || log;
+        let string = log || log.toString();
 		if (!string || string === "[object Object]") {
 			string = JSON.stringify(log);
 		} 
