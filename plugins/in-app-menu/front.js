13c8c55f07709da29c7d1b1ba8e93193e240fc36
Araxeus <oaeben@gmail.com>
2021-04-09 22:28:55 +0300

    Revert "fix rare crash due to unfocus effect"
    This reverts commit 80b12076406e5e06a219f5b7824f1f3821b54832.

--- plugins/in-app-menu/front.js
+++ plugins/in-app-menu/front.js
@@ -6,8 +6,6 @@ module.exports = () => {
 	const bar = new customTitlebar.Titlebar({
 		backgroundColor: customTitlebar.Color.fromHex("#050505"),
 		itemBackgroundColor: customTitlebar.Color.fromHex("#121212"),
-		// !important - unfocus effect can sometimes cause crash as of v3.2.6 of custom-electron-titlebar
-		unfocusEffect: false 
 	});
 	bar.updateTitle(" ");
 	document.title = "Youtube Music";
