caa8b6c47c7729e65df3af531e53562b9bb188f5
Araxeus <oaeben@gmail.com>
2021-04-09 19:10:20 +0300

    change notification priority to show only on linux

--- plugins/notifications/menu.js
+++ plugins/notifications/menu.js
@@ -2,21 +2,17 @@ const { urgencyLevels, setOption } = require("./utils");
 const is = require("electron-is");
 
 module.exports = (win, options) => [
-	{
-		label: "Notification Priority",
-		submenu: urgencyLevels.map(level => ({
-			label: level.name,
-			type: "radio",
-			checked: options.urgency === level.value,
-			click: () => setOption(options, "urgency", level.value)
-		})),
-	},
-	{
-		label: "Show notification on unpause",
-		type: "checkbox",
-		checked: options.unpauseNotification,
-		click: (item) => setOption(options, "unpauseNotification", item.checked)
-	},
+	...(is.linux() ?
+		[{
+			label: "Notification Priority",
+			submenu: urgencyLevels.map(level => ({
+				label: level.name,
+				type: "radio",
+				checked: options.urgency === level.value,
+				click: () => setOption(options, "urgency", level.value)
+			})),
+		}] :
+		[]),
 	...(is.windows() ?
 		[{
 			label: "Interactive Notifications",
@@ -24,5 +20,11 @@ module.exports = (win, options) => [
 			checked: options.interactive,
 			click: (item) => setOption(options, "interactive", item.checked)
 		}] :
-		[])
+		[]),
+	{
+		label: "Show notification on unpause",
+		type: "checkbox",
+		checked: options.unpauseNotification,
+		click: (item) => setOption(options, "unpauseNotification", item.checked)
+	},
 ];
