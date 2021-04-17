49698ea6693462524a88b07b87e82b06e560dda2
Araxeus <oaeben@gmail.com>
2021-04-17 05:08:07 +0300

    fix changing settings when plugin is disabled

--- plugins/precise-volume/menu.js
+++ plugins/precise-volume/menu.js
@@ -1,10 +1,19 @@
+const { enabled } = require("./back")
+const { setOptions } = require("../../config/plugins");
+
 module.exports = (win, options) => [
 	{
 		label: "Arrowkeys controls",
 		type: "checkbox",
 		checked: !!options.arrowsShortcut,
 		click: (item) => {
-            win.webContents.send("setArrowsShortcut", item.checked);
+            //dynamically change setting if plugin enabled
+            if (enabled()) {
+                win.webContents.send("setArrowsShortcut", item.checked);
+            } else { //fallback to usual method if disabled
+                options.arrowsShortcut = item.checked;
+                setOptions("precise-volume", options);
+            }
         }
 	}
 ];
