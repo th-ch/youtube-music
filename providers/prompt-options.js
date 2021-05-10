e43c01da6470d4ff0a2f844b8ac8bc219b2302a7
Araxeus <oaeben@gmail.com>
2021-05-11 00:20:00 +0300

    lint

--- providers/prompt-options.js
+++ providers/prompt-options.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const is = require("electron-is");
 
-const iconPath = path.resolve(__dirname, "../assets/youtube-music-tray.png");
+const iconPath = path.join(__dirname, "..", "assets", "youtube-music-tray.png");
 const customTitlebarPath = path.join(__dirname, "prompt-custom-titlebar.js");
 
 const promptOptions = is.macOS() ? {
@@ -9,7 +9,6 @@ const promptOptions = is.macOS() ? {
     icon: iconPath
 } : {
     customStylesheet: "dark",
-    icon: iconPath,
     // The following are used for custom titlebar
     frame: false,
     customScript: customTitlebarPath,
