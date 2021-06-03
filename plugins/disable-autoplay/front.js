057b88efd6cc939a962ed40a0c9871de5486dbfb
TC <th-ch@users.noreply.github.com>
2021-06-03 21:45:28 +0200

    Refactor videoElement getter into a provider with callback

--- plugins/disable-autoplay/front.js
+++ plugins/disable-autoplay/front.js
@@ -1,25 +1,10 @@
-let videoElement = null;
+const { ontimeupdate } = require("../../providers/video-element");
 
-const observer = new MutationObserver((mutations, observer) => {
-	if (!videoElement) {
-		videoElement = document.querySelector("video");
-	}
-
-	if (videoElement) {
-		videoElement.ontimeupdate = () => {
-			if (videoElement.currentTime === 0 && videoElement.duration !== NaN) {
-				// auto-confirm-when-paused plugin can interfere here if not disabled!
-				videoElement.pause();
-			}
-		};
-	}
-});
-
-function observeVideoElement() {
-	observer.observe(document, {
-		childList: true,
-		subtree: true,
+module.exports = () => {
+	ontimeupdate((videoElement) => {
+		if (videoElement.currentTime === 0 && videoElement.duration !== NaN) {
+			// auto-confirm-when-paused plugin can interfere here if not disabled!
+			videoElement.pause();
+		}
 	});
-}
-
-module.exports = observeVideoElement;
+};
