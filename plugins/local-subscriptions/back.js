c375d608a518776b71481eac44d7399388931bd2
Fyodor Simonov <fdr.smnv@outlook.com>
2021-06-25 23:05:41 +0300

    Refactor html & css, prepare front for sub button

--- plugins/local-subscriptions/back.js
+++ plugins/local-subscriptions/back.js
@@ -1,10 +1,14 @@
-const { BrowserWindow } = require("electron");
+const { BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 
 const { injectCSS, listenAction, templatePath } = require("../utils");
 const { ACTIONS, CHANNEL } = require("./actions.js");
 
 module.exports = win => {
+  injectCSS(win.webContents, path.join(__dirname, "style.css"), () => {
+		win.webContents.send("subscriptions-css-ready");
+	});
+
   listenAction(CHANNEL, (event, action) => {
     switch(action) {
       case ACTIONS.BUTTON:
@@ -15,4 +19,6 @@ module.exports = win => {
           console.log("Unknown action: " + action);
     }
   })
+  win.webContents.on('did-stop-loading', () => console.log('Loaded'))
+  win.webContents.on('did-navigate-in-page', () => win.webContents.send('subscriptions-location-change'));
 };
--- plugins/local-subscriptions/front.js
+++ plugins/local-subscriptions/front.js
@@ -2,7 +2,7 @@ const { ipcRenderer } = require("electron");
 const { ElementFromFile, templatePath } = require("../utils");
 
 module.exports = () => {
-  ipcRenderer.on("navigation-css-ready", () => {
+  ipcRenderer.on('subscriptions-css-ready', () => {
   const localPlaylistsButton = ElementFromFile(templatePath(__dirname, "subscriptions-tab.html"));
   const menu = document.querySelector("ytmusic-pivot-bar-renderer");
 
@@ -10,4 +10,69 @@ module.exports = () => {
     menu.appendChild(localPlaylistsButton);
   }
   });
+
+  // TODO: 
+  // * Listen to 'did-stop-loading' event
+  // * Check if it is a channel page
+  // * Add subscribe button
+  // * Think through how the subscriptions are going to get stored (electron-store, data structures)
+  // * Display subscription list in the popup window
+
+  ipcRenderer.on('subscriptions-location-change', (event) => {
+    const isChannelPage = window.location.pathname.includes('channel');
+    console.log('Is this page a channel?', isChannelPage)
+
+      // const channelButtonsObserver = new MutationObserver((mutations, observer) => {
+      //   const buttons = document.getElementsByClassName(".buttons .style-scope .ytmusic-immersive-header-renderer")[0];
+      //   if (buttons) {
+      //     const localSubscriptionButton = document.createElement('button')
+      //     console.log(buttons);
+      //     buttons.appendChild(localSubscriptionButton);
+      //     observer.disconnect();
+      //   }
+      // })
+      // if (window.location.pathname.includes('channel')) {
+      //   channelButtonsObserver.observe(document, {childList: true, subtree: true});
+      // } else {
+      //   channelButtonsObserver.disconnect();
+      // }
+      // watchDOMElement(
+      //   'buttonsss', 
+      //   (document) => document.getElementsByClassName("div.buttons.style-scope.ytmusic-immersive-header-renderer")[0],
+      //   (buttonsElement, mutations, observer) => {
+      //     if (buttonsElement) {
+      //       const localSubscriptionButton = document.createElement('button')
+      //       console.log(buttonsElement);
+      //       buttonsElement.appendChild(localSubscriptionButton);
+      //       observer.disconnect();
+      //     }
+      //   })
+      // const buttons = document.querySelector("div.buttons.style-scope.ytmusic-immersive-header-renderer");
+      // const localSubscriptionButton = document.createElement('button')
+      // console.log(buttons);
+      // if (buttons) {
+      //   buttons.appendChild(localSubscriptionButton)
+      // } else {
+      //   console.log('No Buttons Found.')
+      // }
+  })
+
+  // ipcRenderer.on('subscriptions-location-change', () => {
+  //   const isChannelPage = window.location.pathname.includes('channel');
+  //   console.log('Is this page a channel?', isChannelPage)
+
+  //   if (isChannelPage) {
+  //     ipcRenderer.on('subscriptions-dom-ready', () => {
+  //       const buttons = document.querySelector("div.buttons.style-scope.ytmusic-immersive-header-renderer");
+  //       const localSubscriptionButton = document.createElement('button')
+  //       console.log(buttons);
+  //       if (buttons) {
+  //         buttons.appendChild(localSubscriptionButton)
+  //       } else {
+  //         console.log('No Buttons Found.')
+  //       }
+  //       ipcRenderer.removeListener('subscriptions-dom-ready');
+  //     })
+  //   }
+  // })
 };
