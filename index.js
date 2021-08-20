9df5d921c719f4e0feeced97b18fa6f6e2a6941a
konhi <hello.konhi@gmail.com>
2021-08-20 12:33:46 +0200

    Chrome -> Firefox, simplified using electron-is
    suggestions by @Araxeus

--- index.js
+++ index.js
@@ -211,19 +211,14 @@ app.once("browser-window-created", (event, win) => {
 			// User agents are from https://developers.whatismybrowser.com/useragents/explore/
 			const userAgents = {
 				mac: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:70.0) Gecko/20100101 Firefox/70.0",
-				windows: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-				linux: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36",
+				windows: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0",
+				linux: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0",
 			}
-
-			const userAgent = ""
-
-			if (process.platform == darwin) {
-				userAgent = userAgents.mac;
-			} else if (process.platform == win32) {
-				userAgent = userAgents.windows;
-			} else {
-				userAgent = userAgents.linux;
-			}
+			
+			const userAgent = 
+				is.macOS() ? userAgents.mac :
+				is.windows() ? userAgents.windows :
+				userAgents.linux;
 
 			win.webContents.session.webRequest.onBeforeSendHeaders((details, cb) => {
 				details.requestHeaders["User-Agent"] = userAgent;
