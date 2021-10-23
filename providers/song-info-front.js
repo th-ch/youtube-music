79a95f133bde84f3fb26e7012ea598933608a36f
Araxeus <78568641+Araxeus@users.noreply.github.com>
2021-10-23 18:22:17 +0300

    use apiLoad event

--- providers/song-info-front.js
+++ providers/song-info-front.js
@@ -4,35 +4,16 @@ const { getImage } = require("./song-info");
 
 global.songInfo = {};
 
-let api = document.querySelector('#movie_player');
-
 ipcRenderer.on("update-song-info", async (_, extractedSongInfo) => {
 	global.songInfo = JSON.parse(extractedSongInfo);
 	global.songInfo.image = await getImage(global.songInfo.imageSrc);
 });
 
-function setup() {
-	if (api) {
-		injectListener();
-		return;
-	}
-
-	const observer = new MutationObserver(() => {
-		api = document.querySelector('#movie_player');
-		if (api) {
-			observer.disconnect();
-			injectListener();
-		}
+module.exports = () => {
+	document.addEventListener('apiLoaded', e => {
+		document.querySelector('video').addEventListener('loadeddata', () => {
+			const data = e.detail.getPlayerResponse();
+			ipcRenderer.send("song-info-request", JSON.stringify(data));
+		});
 	})
-
-	observer.observe(document.documentElement, { childList: true, subtree: true });
-}
-
-function injectListener() {
-	document.querySelector('video').addEventListener('loadeddata', () => {
-		const data = api.getPlayerResponse();
-		ipcRenderer.send("song-info-request", JSON.stringify(data));
-	});
 };
-
-module.exports = setup;
