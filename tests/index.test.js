3f3df098193778907c58ccce089ee3da82523991
TC <th-ch@users.noreply.github.com>
2022-09-05 00:38:24 +0200

    Set NODE_ENV programmatically

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,6 +3,8 @@ const path = require("path");
 const { _electron: electron } = require("playwright");
 const { test, expect } = require("@playwright/test");
 
+process.env.NODE_ENV = "test";
+
 const appPath = path.resolve(__dirname, "..");
 
 test("YouTube Music App - With default settings, app is launched and visible", async () => {
