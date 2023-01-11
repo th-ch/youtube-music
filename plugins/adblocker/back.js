2b7b3de7543633f6eb3946ecb0118cb9c4e21085
Araxeus <78568641+Araxeus@users.noreply.github.com>
2023-01-11 19:18:48 +0200

    experimental faster adblocker v1

--- plugins/adblocker/back.js
+++ plugins/adblocker/back.js
@@ -1,8 +1,8 @@
-const { loadAdBlockerEngine } = require("./blocker");
-module.exports = (win, options) =>
-	loadAdBlockerEngine(
-		win.webContents.session,
-		options.cache,
-		options.additionalBlockLists,
-		options.disableDefaultLists
-	);
+//const { loadAdBlockerEngine } = require("./blocker");
+module.exports = (win, options) => {}
+	// loadAdBlockerEngine(
+	// 	win.webContents.session,
+	// 	options.cache,
+	// 	options.additionalBlockLists,
+	// 	options.disableDefaultLists
+	// );
--- plugins/adblocker/preload.js
+++ plugins/adblocker/preload.js
@@ -1,4 +1,4 @@
 module.exports = () => {
 	// Preload adblocker to inject scripts/styles
-	require("@cliqz/adblocker-electron-preload/dist/preload.cjs");
+	//require("@cliqz/adblocker-electron-preload/dist/preload.cjs");
 };
