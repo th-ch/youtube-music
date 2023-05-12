a9227b529c46924197573eed01d953b9f794907d
TC <th-ch@users.noreply.github.com>
2023-05-12 19:07:51 +0200

    Use 'with blocklists' as default in menu

--- plugins/adblocker/menu.js
+++ plugins/adblocker/menu.js
@@ -6,7 +6,7 @@ module.exports = () => [
 		submenu: Object.values(config.blockers).map((blocker) => ({
 			label: blocker,
 			type: "radio",
-			checked: config.get("blocker") === blocker,
+			checked: (config.get("blocker") || config.blockers.WithBlocklists) === blocker,
 			click: () => {
 				config.set("blocker", blocker);
 			},
