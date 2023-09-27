170e2a696e724c8d3bcb92e6475554782df589c6
JellyBrick <shlee1503@naver.com>
2023-09-27 17:13:51 +0900

    cherry-picked: Fixed canvas injected at the wrong position
    Co-authored-by: MiepHD <63968466+MiepHD@users.noreply.github.com>

--- plugins/visualizer/front.ts
+++ plugins/visualizer/front.ts
@@ -33,9 +33,7 @@ export default (options: ConfigType<'visualizer'>) => {
       if (!canvas) {
         canvas = document.createElement('canvas');
         canvas.id = 'visualizer';
-        canvas.style.position = 'absolute';
-        canvas.style.background = 'black';
-        visualizerContainer.append(canvas);
+        visualizerContainer.prepend(canvas);
       }
 
       const resizeCanvas = () => {
