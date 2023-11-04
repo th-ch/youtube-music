cde08f5b2ac300722c1c3982314473cf6660403a
JellyBrick <shlee1503@naver.com>
2023-11-04 13:48:02 +0900

    fix: download button

--- src/plugins/downloader/front.ts
+++ src/plugins/downloader/front.ts
@@ -11,55 +11,55 @@ const downloadButton = ElementFromHtml(downloadHTML);
 
 let doneFirstLoad = false;
 
-const menuObserver = new MutationObserver(() => {
-  if (!menu) {
-    menu = getSongMenu();
-    if (!menu) {
-      return;
-    }
-  }
-
-  if (menu.contains(downloadButton)) {
-    return;
-  }
-
-  const menuUrl = document.querySelector<HTMLAnchorElement>('tp-yt-paper-listbox [tabindex="0"] #navigation-endpoint')?.href;
-  if (!menuUrl?.includes('watch?') && doneFirstLoad) {
-    return;
-  }
-
-  menu.prepend(downloadButton);
-  progress = document.querySelector('#ytmcustom-download');
-
-  if (doneFirstLoad) {
-    return;
-  }
-
-  setTimeout(() => doneFirstLoad ||= true, 500);
-});
-
-window.download = () => {
-  let videoUrl = getSongMenu()
-    // Selector of first button which is always "Start Radio"
-    ?.querySelector('ytmusic-menu-navigation-item-renderer[tabindex="-1"] #navigation-endpoint')
-    ?.getAttribute('href');
-  if (videoUrl) {
-    if (videoUrl.startsWith('watch?')) {
-      videoUrl = defaultConfig.url + '/' + videoUrl;
-    }
-
-    if (videoUrl.includes('?playlist=')) {
-      window.ipcRenderer.send('download-playlist-request', videoUrl);
-      return;
-    }
-  } else {
-    videoUrl = getSongInfo().url || window.location.href;
-  }
-
-  window.ipcRenderer.send('download-song', videoUrl);
-};
-
 export default () => {
+  const menuObserver = new MutationObserver(() => {
+    if (!menu) {
+      menu = getSongMenu();
+      if (!menu) {
+        return;
+      }
+    }
+
+    if (menu.contains(downloadButton)) {
+      return;
+    }
+
+    const menuUrl = document.querySelector<HTMLAnchorElement>('tp-yt-paper-listbox [tabindex="-1"] #navigation-endpoint')?.href;
+    if (!menuUrl?.includes('watch?') && doneFirstLoad) {
+      return;
+    }
+
+    menu.prepend(downloadButton);
+    progress = document.querySelector('#ytmcustom-download');
+
+    if (doneFirstLoad) {
+      return;
+    }
+
+    setTimeout(() => doneFirstLoad ||= true, 500);
+  });
+
+  window.download = () => {
+    let videoUrl = getSongMenu()
+      // Selector of first button which is always "Start Radio"
+      ?.querySelector('ytmusic-menu-navigation-item-renderer[tabindex="-1"] #navigation-endpoint')
+      ?.getAttribute('href');
+    if (videoUrl) {
+      if (videoUrl.startsWith('watch?')) {
+        videoUrl = defaultConfig.url + '/' + videoUrl;
+      }
+
+      if (videoUrl.includes('?playlist=')) {
+        window.ipcRenderer.send('download-playlist-request', videoUrl);
+        return;
+      }
+    } else {
+      videoUrl = getSongInfo().url || window.location.href;
+    }
+
+    window.ipcRenderer.send('download-song', videoUrl);
+  };
+
   document.addEventListener('apiLoaded', () => {
     menuObserver.observe(document.querySelector('ytmusic-popup-container')!, {
       childList: true,
