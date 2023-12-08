350dacc1a574a37f0f1316be76180898e027fb19
Yazazuyo <63968466+MiepHD@users.noreply.github.com>
2023-12-08 21:20:36 +0100

    Update src/plugins/skip-disliked-songs/index.ts
    Co-authored-by: JellyBrick <shlee1503@naver.com>

--- src/plugins/skip-disliked-songs/index.ts
+++ src/plugins/skip-disliked-songs/index.ts
@@ -1,34 +1,41 @@
-import { t } from '@/i18n';
-import { createPlugin } from '@/utils';
+import { t } from "@/i18n";
+import { createPlugin } from "@/utils";
 
 export default createPlugin({
-    name: () => t('plugins.skip-disliked-songs.name'),
-    description: () => t('plugins.skip-disliked-songs.description'),
-    restartNeeded: false,
-    renderer: {
-        observer: null as MutationObserver | null,
-        async start() {
-            const likeBtn = await this.waitForElem('#like-button-renderer');
-            this.observer = new MutationObserver(() => {
-                if (likeBtn?.getAttribute("like-status") == "DISLIKE")
-                    document.querySelector("tp-yt-paper-icon-button.next-button")?.click();
-            });
-            this.observer.observe(likeBtn,
-                { attributes: true, childList: false, subtree: false });
-        },
-        stop() {
-            this.observer?.disconnect();
-        },
-        waitForElem(selector) {
-            return new Promise(resolve => {
-                const interval = setInterval(() => {
-                    const elem = document.querySelector(selector);
-                    if (!elem) return;
-        
-                    clearInterval(interval);
-                    resolve(elem);
-                });
-            });
-        }
-    }
+  name: () => t("plugins.skip-disliked-songs.name"),
+  description: () => t("plugins.skip-disliked-songs.description"),
+  restartNeeded: false,
+  renderer: {
+    observer: null as MutationObserver | null,
+    start() {
+      this.waitForElem("#like-button-renderer").then((likeBtn) => {
+        this.observer = new MutationObserver(() => {
+          if (likeBtn?.getAttribute("like-status") == "DISLIKE") {
+            document
+              .querySelector("tp-yt-paper-icon-button.next-button")
+              ?.click();
+          }
+        });
+        this.observer.observe(likeBtn, {
+          attributes: true,
+          childList: false,
+          subtree: false,
+        });
+      });
+    },
+    stop() {
+      this.observer?.disconnect();
+    },
+    waitForElem(selector) {
+      return new Promise((resolve) => {
+        const interval = setInterval(() => {
+          const elem = document.querySelector(selector);
+          if (!elem) return;
+
+          clearInterval(interval);
+          resolve(elem);
+        });
+      });
+    },
+  },
 });
