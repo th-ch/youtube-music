cff3172121f948da2dc62b25c4722589e4c9b3fd
MiepHD <63968466+MiepHD@users.noreply.github.com>
2023-12-08 20:27:02 +0100

    Changed it to activate and deactivate without restart

--- src/plugins/skip-disliked-songs/index.ts
+++ src/plugins/skip-disliked-songs/index.ts
@@ -4,13 +4,19 @@ import { createPlugin } from '@/utils';
 export default createPlugin({
     name: () => t('plugins.skip-disliked-songs.name'),
     description: () => t('plugins.skip-disliked-songs.description'),
-    restartNeeded: true,
-    config: {
-      enabled: true,
-    },
-    renderer() {
-        new MutationObserver(() => {
-            if (document.querySelector("#like-button-renderer")?.getAttribute("like-status") == "DISLIKE") document.querySelector("tp-yt-paper-icon-button.next-button")?.click();
-        }).observe(document.querySelector("#like-button-renderer"), { attributes: true, childList: false, subtree: false });
-    },
+    restartNeeded: false,
+    renderer: {
+        observer: MutationObserver,
+        async start() {
+            this.observer = new MutationObserver(() => {
+                if (document.querySelector("#like-button-renderer")?.getAttribute("like-status") == "DISLIKE")
+                    document.querySelector("tp-yt-paper-icon-button.next-button")?.click();
+            });
+            this.observer.observe(document.querySelector("#like-button-renderer"),
+                { attributes: true, childList: false, subtree: false });
+        },
+        stop() {
+            this.observer.disconnect();
+        }
+    }
   });  
