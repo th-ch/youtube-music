7e5dbcc7660eeadd9c9b82f6ffe6e06c7b9abd2a
MiepHD <63968466+MiepHD@users.noreply.github.com>
2023-12-09 14:29:30 +0100

    Added album options

---
+++ src/plugins/album-actions/index.ts
@@ -0,0 +1,103 @@
+import { t } from '@/i18n';
+import { createPlugin } from '@/utils';
+import { ElementFromHtml } from '@/plugins/utils/renderer';
+
+import undislikeHTML from './templates/undislike.html?raw';
+import dislikeHTML from './templates/dislike.html?raw';
+import likeHTML from './templates/like.html?raw';
+import unlikeHTML from './templates/unlike.html?raw';
+
+export default createPlugin({
+  name: () => t('plugins.album-actions.name'),
+  description: () => t('plugins.album-actions.description'),
+  restartNeeded: false,
+  renderer: {
+    observer: null as MutationObserver | null,
+    start() {
+      this.onPageChange();
+      this.waitForElem('#browse-page').then((page: HTMLElement) => {
+        this.observer = new MutationObserver(() => {
+          this.onPageChange();
+        });
+        this.observer.observe(page, {
+          attributes: false,
+          childList: true,
+          subtree: true,
+        });
+      });
+    },
+    onPageChange() {
+      console.log('Running 1');
+      const buttons: Array<HTMLElement> = [
+        ElementFromHtml(undislikeHTML),
+        ElementFromHtml(dislikeHTML),
+        ElementFromHtml(likeHTML),
+        ElementFromHtml(unlikeHTML),
+      ];
+      const menu = document.querySelector('.detail-page-menu');
+      if (menu && !document.querySelector('.like-menu')) {
+        console.log('Running 2');
+        for (const button of buttons) {
+          menu.appendChild(button);
+          button.addEventListener('click', this.applyToList);
+        }
+      }
+    },
+    applyToList(event) {
+      event.stopPropagation();
+      let playlistbuttons: NodeListOf<Element> | undefined;
+      switch (event.currentTarget.id) {
+        case 'allundislike':
+          playlistbuttons = document
+            .querySelector('ytmusic-shelf-renderer')
+            ?.querySelectorAll(
+              '#button-shape-dislike[aria-pressed=true] > button',
+            );
+          break;
+        case 'alldislike':
+          playlistbuttons = document
+            .querySelector('ytmusic-shelf-renderer')
+            ?.querySelectorAll(
+              '#button-shape-dislike[aria-pressed=false] > button',
+            );
+          break;
+        case 'alllike':
+          playlistbuttons = document
+            .querySelector('ytmusic-shelf-renderer')
+            ?.querySelectorAll(
+              '#button-shape-like[aria-pressed=false] > button',
+            );
+          break;
+        case 'allunlike':
+          playlistbuttons = document
+            .querySelector('ytmusic-shelf-renderer')
+            ?.querySelectorAll(
+              '#button-shape-like[aria-pressed=true] > button',
+            );
+          break;
+        default:
+      }
+      if (playlistbuttons)
+        for (const elem of playlistbuttons) {
+          elem.click();
+        }
+    },
+    stop() {
+      this.observer?.disconnect();
+      for (const button of document.querySelectorAll('.like-menu')) {
+        button.remove();
+      }
+    },
+    waitForElem(selector: string): Promise<Element> {
+      return new Promise((resolve) => {
+        const interval = setInterval(() => {
+          const elem = document.querySelector(selector);
+          if (!elem) return;
+
+          clearInterval(interval);
+          resolve(elem);
+        });
+      });
+    },
+  },
+});
