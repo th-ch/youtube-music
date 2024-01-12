cfaab8fbc4137492a50b8b89faa44d04f1cc4fe1
JellyBrick <shlee1503@naver.com>
2024-01-13 08:16:02 +0900

    chore: add namespace for ipc

--- src/providers/song-controls.ts
+++ src/providers/song-controls.ts
@@ -4,32 +4,32 @@ import { BrowserWindow, ipcMain } from 'electron';
 export default (win: BrowserWindow) => {
   const commands = {
       // Playback
-      previous: () => win.webContents.send('previous-video'),
-      next: () => win.webContents.send('next-video'),
-      playPause: () => win.webContents.send('toggle-play'),
-      like: () => win.webContents.send('update-like', 'LIKE'),
-      dislike: () => win.webContents.send('update-like', 'DISLIKE'),
-      go10sBack: () => win.webContents.send('seekBy', -10),
-      go10sForward: () => win.webContents.send('seekBy', 10),
-      go1sBack: () => win.webContents.send('seekBy', -1),
-      go1sForward: () => win.webContents.send('seekBy', 1),
-      shuffle: () => win.webContents.send('shuffle'),
-      switchRepeat: (n = 1) => win.webContents.send('switch-repeat', n),
+      previous: () => win.webContents.send('ytmd:previous-video'),
+      next: () => win.webContents.send('ytmd:next-video'),
+      playPause: () => win.webContents.send('ytmd:toggle-play'),
+      like: () => win.webContents.send('ytmd:update-like', 'LIKE'),
+      dislike: () => win.webContents.send('ytmd:update-like', 'DISLIKE'),
+      go10sBack: () => win.webContents.send('ytmd:seek-by', -10),
+      go10sForward: () => win.webContents.send('ytmd:seek-by', 10),
+      go1sBack: () => win.webContents.send('ytmd:seek-by', -1),
+      go1sForward: () => win.webContents.send('ytmd:seek-by', 1),
+      shuffle: () => win.webContents.send('ytmd:shuffle'),
+      switchRepeat: (n = 1) => win.webContents.send('ytmd:switch-repeat', n),
       // General
       volumeMinus10: () => {
-        ipcMain.once('get-volume-return', (_, volume) => {
-          win.webContents.send('update-volume', volume - 10);
+        ipcMain.once('ytmd:get-volume-return', (_, volume) => {
+          win.webContents.send('ytmd:update-volume', volume - 10);
         });
-        win.webContents.send('get-volume');
+        win.webContents.send('ytmd:get-volume');
       },
       volumePlus10: () => {
-        ipcMain.once('get-volume-return', (_, volume) => {
-          win.webContents.send('update-volume', volume + 10);
+        ipcMain.once('ytmd:get-volume-return', (_, volume) => {
+          win.webContents.send('ytmd:update-volume', volume + 10);
         });
-        win.webContents.send('get-volume');
+        win.webContents.send('ytmd:get-volume');
       },
-      fullscreen: () => win.webContents.send('toggle-fullscreen'),
-      muteUnmute: () => win.webContents.send('toggle-mute'),
+      fullscreen: () => win.webContents.send('ytmd:toggle-fullscreen'),
+      muteUnmute: () => win.webContents.send('ytmd:toggle-mute'),
   };
   return {
     ...commands,
