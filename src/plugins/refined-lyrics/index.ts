41ac6a7286d51ecf4808de6d354d04c8f5d9ae2d
Angelos Bouklis <53124886+ArjixWasTaken@users.noreply.github.com>
2024-02-22 00:21:05 +0200

    chore: clean-up code

--- src/plugins/refined-lyrics/index.ts
+++ src/plugins/refined-lyrics/index.ts
@@ -4,9 +4,7 @@ import { createPlugin } from '@/utils';
 import { RefinedLyricsConfig } from './config';
 import { YoutubePlayer } from '@/types/youtube-player';
 import { VideoDataChangeValue } from '@/types/player-api-events';
-import { selectors, waitForElement } from './utils';
-
-import { debounce, throttle } from '@/providers/decorators';
+import { enableTab, selectors, tabStates } from './utils';
 
 // Temporary hack to not mess with translations for now.
 // prettier-ignore
@@ -16,15 +14,6 @@ type VideoDataChangedCallback = (
   type: string,
   data: VideoDataChangeValue,
 ) => void;
-
-const tabStates = {
-  true: debounce(({ author: _author, title: _title }: VideoDataChangeValue) => {
-    // TODO: This is where we fetch lyrics.
-  }, 200),
-  false: debounce(() => {
-    // TODO: Should we do anything here?
-  }, 200),
-};
 
 export default createPlugin({
   name: t`Refined Lyrics`,
@@ -44,24 +33,16 @@ export default createPlugin({
     },
 
     // prettier-ignore
-    videoDataChange(type: string, data: VideoDataChangeValue) {
-      const header = document.querySelector(selectors.head);
+    videoDataChange(_type: string, data: VideoDataChangeValue) {
+      const header = document.querySelector<HTMLElement>(selectors.head);
       if (!header) return;
-
-      const enableTab = throttle(() => {
-        if (header?.hasAttribute('disabled')) {
-          header.removeAttribute('disabled');
-        }
-      }, 80);
 
       // Force the lyrics tab to be enabled at all times.
       new MutationObserver((mutations) => {
-          enableTab();
-
           for (const mutation of mutations) {
             switch (mutation.attributeName) {
               case 'disabled':
-                enableTab();
+                enableTab(header);
                 break;
               case 'aria-selected':
                 // @ts-expect-error I know what I am doing, fuck off TypeSript
