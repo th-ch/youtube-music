a66655b3937fa1669f5a88da7c8a1c6cead4296c
Angelos Bouklis <53124886+ArjixWasTaken@users.noreply.github.com>
2024-02-22 00:35:07 +0200

    chore: decouple logic

--- src/plugins/refined-lyrics/index.ts
+++ src/plugins/refined-lyrics/index.ts
@@ -4,7 +4,7 @@ import { createPlugin } from '@/utils';
 import { RefinedLyricsConfig } from './config';
 import { YoutubePlayer } from '@/types/youtube-player';
 import { VideoDataChangeValue } from '@/types/player-api-events';
-import { enableTab, selectors, tabStates } from './utils';
+import { toggleTab, selectors, tabStates } from './utils';
 
 // Temporary hack to not mess with translations for now.
 // prettier-ignore
@@ -25,8 +25,36 @@ export default createPlugin({
 
   renderer: {
     api: <YoutubePlayer | null>null,
+    observer: new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        const header = <HTMLElement>mutation.target;
+
+        switch (mutation.attributeName) {
+          case 'disabled':
+            toggleTab(header);
+            break;
+          case 'aria-selected':
+            // @ts-expect-error I know what I am doing, fuck off TypeSript
+            tabStates[header.ariaSelected]?.(data);
+            break;
+        }
+      }
+    }),
+
     onPlayerApiReady(api) {
+      this.api = api;
+
       api.addEventListener(
+        'videodatachange',
+        <VideoDataChangedCallback>this.videoDataChange,
+      );
+    },
+
+    start() {},
+    stop() {
+      this.observer.disconnect();
+
+      this.api?.removeEventListener(
         'videodatachange',
         <VideoDataChangedCallback>this.videoDataChange,
       );
@@ -38,22 +66,8 @@ export default createPlugin({
       if (!header) return;
 
       // Force the lyrics tab to be enabled at all times.
-      new MutationObserver((mutations) => {
-          for (const mutation of mutations) {
-            switch (mutation.attributeName) {
-              case 'disabled':
-                enableTab(header);
-                break;
-              case 'aria-selected':
-                // @ts-expect-error I know what I am doing, fuck off TypeSript
-                tabStates[header.ariaSelected]?.(data);
-                break;
-            }
-          }
-
-          // TODO: Are observers automatically destroyed if the node they are watching is also destroyed?
-        },
-      ).observe(header, { attributes: true });
+      this.observer.disconnect();
+      this.observer.observe(header, { attributes: true });
     },
   },
 });
