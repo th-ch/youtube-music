4764ab1f1567c08690d52eeef402a874ddcc95cb
Su-Yong <simssy2205@gmail.com>
2024-03-31 02:48:58 +0900

    feat(api-server): add more API

--- src/plugins/api-server/backend/routes/control.ts
+++ src/plugins/api-server/backend/routes/control.ts
@@ -2,13 +2,42 @@ import { createRoute } from '@hono/zod-openapi';
 
 import getSongControls from '@/providers/song-controls';
 
-import { AuthHeadersSchema } from '../scheme';
+import { AuthHeadersSchema, ResponseSongInfo, SongInfoSchema } from '../scheme';
 
+import type { SongInfo } from '@/providers/song-info';
 import type { BackendContext } from '@/types/contexts';
 import type { APIServerConfig } from '../../config';
 import type { HonoApp } from '../types';
 
 const routes = {
+  previous: createRoute({
+    method: 'post',
+    path: '/api/previous',
+    summary: 'play previous song',
+    description: '',
+    request: {
+      headers: AuthHeadersSchema,
+    },
+    responses: {
+      204: {
+        description: 'Success',
+      },
+    },
+  }),
+  next: createRoute({
+    method: 'post',
+    path: '/api/next',
+    summary: 'play next song',
+    description: '',
+    request: {
+      headers: AuthHeadersSchema,
+    },
+    responses: {
+      204: {
+        description: 'Success',
+      },
+    },
+  }),
   play: createRoute({
     method: 'post',
     path: '/api/play',
@@ -51,11 +80,74 @@ const routes = {
       },
     },
   }),
+  like: createRoute({
+    method: 'post',
+    path: '/api/like',
+    summary: 'like song',
+    description: '',
+    request: {
+      headers: AuthHeadersSchema,
+    },
+    responses: {
+      204: {
+        description: 'Success',
+      },
+    },
+  }),
+  dislike: createRoute({
+    method: 'post',
+    path: '/api/dislike',
+    summary: 'play dislike song',
+    description: '',
+    request: {
+      headers: AuthHeadersSchema,
+    },
+    responses: {
+      204: {
+        description: 'Success',
+      },
+    },
+  }),
+
+  info: createRoute({
+    method: 'get',
+    path: '/api/info',
+    summary: 'get current song info',
+    description: '',
+    request: {
+      headers: AuthHeadersSchema,
+    },
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: SongInfoSchema,
+          }
+        },
+      },
+      204: {
+        description: 'No song info',
+      },
+    },
+  }),
 };
 
-export const register = (app: HonoApp, { window }: BackendContext<APIServerConfig>) => {
+export const register = (app: HonoApp, { window }: BackendContext<APIServerConfig>, songInfoGetter: () => SongInfo | undefined) => {
   const controller = getSongControls(window);
 
+  app.openapi(routes.previous, (ctx) => {
+    controller.previous();
+
+    ctx.status(204);
+    return ctx.body(null);
+  });
+  app.openapi(routes.previous, (ctx) => {
+    controller.previous();
+
+    ctx.status(204);
+    return ctx.body(null);
+  });
   app.openapi(routes.play, (ctx) => {
     controller.play();
 
@@ -73,5 +165,32 @@ export const register = (app: HonoApp, { window }: BackendContext<APIServerConfi
 
     ctx.status(204);
     return ctx.body(null);
+  });
+  app.openapi(routes.like, (ctx) => {
+    controller.like();
+
+    ctx.status(204);
+    return ctx.body(null);
+  });
+  app.openapi(routes.dislike, (ctx) => {
+    controller.dislike();
+
+    ctx.status(204);
+    return ctx.body(null);
+  });
+
+  app.openapi(routes.info, (ctx) => {
+    const info = songInfoGetter();
+
+    if (!info) {
+      ctx.status(204);
+      return ctx.body(null);
+    }
+
+    const body = { ...info };
+    delete body.image;
+
+    ctx.status(200);
+    return ctx.json(body satisfies ResponseSongInfo);
   });
 };
--- src/plugins/api-server/backend/types.ts
+++ src/plugins/api-server/backend/types.ts
@@ -3,12 +3,14 @@ import { serve } from '@hono/node-server';
 
 import type { BackendContext } from '@/types/contexts';
 import type { APIServerConfig } from '@/plugins/api-server/config';
+import type { SongInfo } from '@/providers/song-info';
 
 export type HonoApp = Hono;
 export type BackendType = {
   app?: HonoApp;
   server?: ReturnType<typeof serve>;
   oldConfig?: APIServerConfig;
+  songInfo?: SongInfo;
 
   init: (ctx: BackendContext<APIServerConfig>) => void;
   run: (hostname: string, port: number) => void;
