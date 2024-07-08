6e17a9a9a840a8ae84e641f586e447bfe3e64ed5
Non0reo <pieroni.nolhan@gmail.com>
2024-07-08 15:45:47 +0200

    Changed file indent

--- src/plugins/synced-lyrics/renderer/lyrics/fetch.ts
+++ src/plugins/synced-lyrics/renderer/lyrics/fetch.ts
@@ -5,75 +5,75 @@ import { syncedLyricList, config } from "../renderer";
 export let hadSecondAttempt = false;
     
 export const extractTimeAndText = (line: string, index: number): LineLyrics|null => {
-    const match = /\[(\d+):(\d+)\.(\d+)\](.+)/.exec(line);
-    if (!match) return null;
+  const match = /\[(\d+):(\d+)\.(\d+)\](.+)/.exec(line);
+  if (!match) return null;
 
-    const minutes = parseInt(match[1]);
-    const seconds = parseInt(match[2]);
-    const milliseconds = parseInt(match[3]);
-    const text = match[4] === ' ' ? config.defaultTextString : match[4].slice(1);
-    
-    const time = `${minutes}:${seconds}:${milliseconds}`;
-    const timeInMs = (minutes * 60 * 1000) + (seconds * 1000) + milliseconds;
+  const minutes = parseInt(match[1]);
+  const seconds = parseInt(match[2]);
+  const milliseconds = parseInt(match[3]);
+  const text = match[4] === ' ' ? config.defaultTextString : match[4].slice(1);
+  
+  const time = `${minutes}:${seconds}:${milliseconds}`;
+  const timeInMs = (minutes * 60 * 1000) + (seconds * 1000) + milliseconds;
 
-    return {
-        index,
-        time,
-        timeInMs,
-        text,
-        status: 'upcoming',
-    } as LineLyrics;
+  return {
+    index,
+    time,
+    timeInMs,
+    text,
+    status: 'upcoming',
+  } as LineLyrics;
 }
 
 export const makeLyricsRequest = async (extractedSongInfo: SongInfo): Promise<Array<LineLyrics> | null>  => {
-    const songTitle = `${extractedSongInfo.title}`;
-    const songArtist = `${extractedSongInfo.artist}`;
-    const songAlbum = extractedSongInfo.album ? `${extractedSongInfo.album}` : undefined;
-    const songDuration = extractedSongInfo.songDuration;
-    
-    return await getLyricsList(songTitle, songArtist, songAlbum, songDuration);
+  const songTitle = `${extractedSongInfo.title}`;
+  const songArtist = `${extractedSongInfo.artist}`;
+  const songAlbum = extractedSongInfo.album ? `${extractedSongInfo.album}` : undefined;
+  const songDuration = extractedSongInfo.songDuration;
+  
+  return await getLyricsList(songTitle, songArtist, songAlbum, songDuration);
 }
     
 export const getLyricsList = async (songTitle: string, songArtist: string, songAlbum: string|undefined, songDuration: number): Promise<Array<LineLyrics> | null> => {
-    let url =  `https://lrclib.net/api/search?artist_name=${encodeURIComponent(songArtist)}&track_name=${encodeURIComponent(songTitle)}`
-    if (songAlbum !== undefined) url += `&album_name=${encodeURIComponent(songAlbum)}`;
+  let url =  `https://lrclib.net/api/search?artist_name=${encodeURIComponent(songArtist)}&track_name=${encodeURIComponent(songTitle)}`
+  if (songAlbum !== undefined) url += `&album_name=${encodeURIComponent(songAlbum)}`;
 
-    const response = await fetch(url);
-    if (!response.ok)
-    return null;
+  const response = await fetch(url);
+  if (!response.ok)
+  return null;
 
-    return await response.json().then(async (data: any) => { 
-        let dataIndex: number = 0;
-        if (!data.length && config.showLyricsEvenIfInexact) { //If no lyrics are found, try again with a different search query
-            hadSecondAttempt = true;
-            const secondResponse = await fetch(`https://lrclib.net/api/search?q=${encodeURIComponent(songTitle)}`);
-            if (!secondResponse.ok) return null;
-            data = await secondResponse.json();
+  return await response.json().then(async (data: any) => { 
+    let dataIndex: number = 0;
+    if (!data.length && config.showLyricsEvenIfInexact) { //If no lyrics are found, try again with a different search query
+      hadSecondAttempt = true;
+      const secondResponse = await fetch(`https://lrclib.net/api/search?q=${encodeURIComponent(songTitle)}`);
+      if (!secondResponse.ok) return null;
+      data = await secondResponse.json();
+    }
+    else if (!data.length) 
+      return null;
+
+    let songsWithMatchingArtist = data.filter((song: any) => songArtist.toLowerCase().includes(song.artistName.toLowerCase()) && song.syncedLyrics); //Lowercase to avoid case sensitivity from API
+    if (!songsWithMatchingArtist.length) return null;
+    dataIndex = 0;
+    if (songsWithMatchingArtist.length > 1) {
+      for (let i = 0; i < songsWithMatchingArtist.length; i++) {
+        if (Math.abs(songsWithMatchingArtist[i].duration - songDuration) < Math.abs(songsWithMatchingArtist[dataIndex].duration - songDuration)) {
+          dataIndex = i;
         }
-        else if (!data.length) 
-            return null;
+      }
+    }
 
-        let songsWithMatchingArtist = data.filter((song: any) => songArtist.toLowerCase().includes(song.artistName.toLowerCase()) && song.syncedLyrics); //Lowercase to avoid case sensitivity from API
-        if (!songsWithMatchingArtist.length) return null;
-        dataIndex = 0;
-        if (songsWithMatchingArtist.length > 1) {
-            for (let i = 0; i < songsWithMatchingArtist.length; i++) {
-                if (Math.abs(songsWithMatchingArtist[i].duration - songDuration) < Math.abs(songsWithMatchingArtist[dataIndex].duration - songDuration)) {
-                    dataIndex = i;
-                }
-            }
-        }
+    data = songsWithMatchingArtist;
+    if (Math.abs(data[dataIndex].duration - songDuration) > 5) return null;
 
-        data = songsWithMatchingArtist;
-        if (Math.abs(data[dataIndex].duration - songDuration) > 5) return null;
-
-        let raw = data[dataIndex].syncedLyrics.split('\n') //Separate the lyrics into lines
-        raw.unshift('[0:0.0] ') //Add a blank line at the beginning
-        raw.forEach((line: string, index: number) => {
-            const syncedLyrics = extractTimeAndText(line, index);
-            if (syncedLyrics !== null) syncedLyricList.push(syncedLyrics);
-        });
-
-        return syncedLyricList;
+    let raw = data[dataIndex].syncedLyrics.split('\n') //Separate the lyrics into lines
+    raw.unshift('[0:0.0] ') //Add a blank line at the beginning
+    raw.forEach((line: string, index: number) => {
+      const syncedLyrics = extractTimeAndText(line, index);
+      if (syncedLyrics !== null) syncedLyricList.push(syncedLyrics);
     });
+
+    return syncedLyricList;
+  });
 };
--- src/plugins/synced-lyrics/renderer/lyrics/insert.ts
+++ src/plugins/synced-lyrics/renderer/lyrics/insert.ts
@@ -2,87 +2,87 @@ import { LineLyrics, LineLyricsStatus } from "../..";
 import { config, hadSecondAttempt, songWithLyrics, syncedLyricList } from "../renderer";
 
 export const initLyricsStyle = () => {
-    let root = document.documentElement;
-    switch (config.lineEffect) { // Set the line effect
-      case 'scale':
+  let root = document.documentElement;
+  switch (config.lineEffect) { // Set the line effect
+    case 'scale':
       root.style.setProperty('--previous-lyrics', 'var(--ytmusic-text-primary)');
       root.style.setProperty('--current-lyrics', 'var(--ytmusic-text-primary)');
       root.style.setProperty('--size-lyrics', '1.2em');
       root.style.setProperty('--offset-lyrics', '0');
       break;
-      case 'offset':
+    case 'offset':
       root.style.setProperty('--previous-lyrics', 'var(--ytmusic-text-primary)');
       root.style.setProperty('--current-lyrics', 'var(--ytmusic-text-primary)');
       root.style.setProperty('--size-lyrics', '1em');
       root.style.setProperty('--offset-lyrics', '1em');
       break;
-      case 'focus':
+    case 'focus':
       root.style.setProperty('--previous-lyrics', 'var(--ytmusic-text-secondary)');
       root.style.setProperty('--current-lyrics', 'var(--ytmusic-text-primary)');
       root.style.setProperty('--size-lyrics', '1em');
       root.style.setProperty('--offset-lyrics', '0');
       break;
-    }
+  }
 };
 
 export const styleLyrics = (actualLyric: LineLyrics) => {
-    const lyrics = document.querySelectorAll('.synced-line');
-    
-    const setStatus = (lyric: Element, status: LineLyricsStatus) => {
-        lyric.classList.remove('current');
-        lyric.classList.remove('previous');
-        lyric.classList.remove('upcoming');
-        lyric.classList.add(status);
-    }
+  const lyrics = document.querySelectorAll('.synced-line');
+  
+  const setStatus = (lyric: Element, status: LineLyricsStatus) => {
+    lyric.classList.remove('current');
+    lyric.classList.remove('previous');
+    lyric.classList.remove('upcoming');
+    lyric.classList.add(status);
+  }
 
-    lyrics.forEach((lyric: Element) => {
-    const index = parseInt(lyric.getAttribute('data-index')!);
-    if (index === actualLyric.index)
-        setStatus(lyric, 'current');
-    else if (index < actualLyric.index)
-        setStatus(lyric, 'previous');
-    else 
-        setStatus(lyric, 'upcoming');
-    });
+  lyrics.forEach((lyric: Element) => {
+  const index = parseInt(lyric.getAttribute('data-index')!);
+  if (index === actualLyric.index)
+    setStatus(lyric, 'current');
+  else if (index < actualLyric.index)
+    setStatus(lyric, 'previous');
+  else 
+    setStatus(lyric, 'upcoming');
+  });
 
-    const targetElement = document.querySelector<HTMLElement>('.current');
-    if (targetElement)
-        targetElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    
+  const targetElement = document.querySelector<HTMLElement>('.current');
+  if (targetElement)
+    targetElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  
 };
 
 
 export const setLyrics = (lyricsContainer: Element, lyrics: Array<LineLyrics> | null) => {
-    let lineList = [];
-    if (lyrics) {
-        const footer = lyricsContainer.querySelector('.footer');
+  let lineList = [];
+  if (lyrics) {
+    const footer = lyricsContainer.querySelector('.footer');
 
-        let lyricsBegin = syncedLyricList[1].timeInMs < 1000 ? 1 : 0; //If the first real lyric is before 1 second, we skip the first blank line
-        for(let i = lyricsBegin; i < syncedLyricList.length; i++) {
-            const line = syncedLyricList[i];
-            lineList.push(`
-            <div class="synced-line ${line.status}" data-index="${line.index}">
-                <span class="text-lyrics">${config.showTimeCodes ? `[${line.time}] ` : ''}${line.text}</span>
-            </div>
-            `);
-        }
-        
-        lyricsContainer.innerHTML = `
-            ${hadSecondAttempt ? '<div class="warning-lyrics">The lyrics for this song may not be exact</div>' : ''}
-            ${lineList.join('')}
-            <span class="footer style-scope ytmusic-description-shelf-renderer" style="align-self: baseline">Source: LRCLIB</span>
-            <yt-formatted-string class="footer style-scope ytmusic-description-shelf-renderer" style="align-self: baseline">
-            Source: LRCLIB
-            </yt-formatted-string>
-        `;
-        lyricsContainer.classList.add('synced-lyrics');
-        lyricsContainer.classList.add('description');
-
-        if (footer) 
-            footer.textContent = 'Source: LRCLIB';
+    let lyricsBegin = syncedLyricList[1].timeInMs < 1000 ? 1 : 0; //If the first real lyric is before 1 second, we skip the first blank line
+    for(let i = lyricsBegin; i < syncedLyricList.length; i++) {
+      const line = syncedLyricList[i];
+      lineList.push(`
+      <div class="synced-line ${line.status}" data-index="${line.index}">
+          <span class="text-lyrics">${config.showTimeCodes ? `[${line.time}] ` : ''}${line.text}</span>
+      </div>
+      `);
     }
+    
+    lyricsContainer.innerHTML = `
+      ${hadSecondAttempt ? '<div class="warning-lyrics">The lyrics for this song may not be exact</div>' : ''}
+      ${lineList.join('')}
+      <span class="footer style-scope ytmusic-description-shelf-renderer" style="align-self: baseline">Source: LRCLIB</span>
+      <yt-formatted-string class="footer style-scope ytmusic-description-shelf-renderer" style="align-self: baseline">
+      Source: LRCLIB
+      </yt-formatted-string>
+    `;
+    lyricsContainer.classList.add('synced-lyrics');
+    lyricsContainer.classList.add('description');
 
-    let root = document.documentElement;
-    if(songWithLyrics) root.style.setProperty('--global-margin', '0.7rem');
-    else root.style.setProperty('--global-margin', '1.4rem'); //Music videos make the space beteween lyrics smaller, so we need to increase the margin
+    if (footer) 
+      footer.textContent = 'Source: LRCLIB';
+  }
+
+  let root = document.documentElement;
+  if(songWithLyrics) root.style.setProperty('--global-margin', '0.7rem');
+  else root.style.setProperty('--global-margin', '1.4rem'); //Music videos make the space beteween lyrics smaller, so we need to increase the margin
 };
