2eb52ff3958659840ce315736dc09ab799672925
Angelos Bouklis <53124886+ArjixWasTaken@users.noreply.github.com>
2024-07-21 04:56:39 +0300

    chore: remove useless signal

--- src/plugins/synced-lyrics/renderer/renderer.tsx
+++ src/plugins/synced-lyrics/renderer/renderer.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/order */
 
-import { createSignal, onCleanup, Show } from 'solid-js';
+import { createSignal, Show } from 'solid-js';
 import { VideoDetails } from '@/types/video-details';
 import { SyncedLyricsPluginConfig } from '../types';
 import { LyricsContainer } from './components/LyricsContainer';
@@ -13,11 +13,6 @@ export const [config, setConfig] = createSignal<SyncedLyricsPluginConfig | null>
 export const [playerState, setPlayerState] = createSignal<VideoDetails | null>(null);
 
 export const LyricsRenderer = () => {
-  const [count, setCount] = createSignal(0);
-
-  const timer = setInterval(() => setCount(count() + 1), 1000);
-  onCleanup(() => clearInterval(timer));
-
   return (
     <Show when={isVisible()}>
       <LyricsContainer />
