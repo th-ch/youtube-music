8ac04ae8ddc05c98d27e9db954d4366a69146630
Angelos Bouklis <53124886+ArjixWasTaken@users.noreply.github.com>
2024-09-22 02:37:34 +0300

    fighting with solid's reactivity x1

--- src/plugins/synced-lyrics/renderer/components/LyricsContainer.tsx
+++ src/plugins/synced-lyrics/renderer/components/LyricsContainer.tsx
@@ -10,55 +10,34 @@ import { SyncedLine } from './SyncedLine';
 
 // import { t } from '@/i18n';
 // import { getSongInfo } from '@/providers/song-info-front';
-import { providerIdx } from './LyricsPicker';
-
 import {
-  // fetchLyrics,
-  providers,
-  searchResults,
-} from '../../providers';
+  isDone,
+  isError,
+  isLoading,
+  lyricsSource,
+  lyricsSourceState,
+} from './LyricsPicker';
 
 import type { LineLyrics } from '../../types';
 import { t } from '@/i18n';
+import { renderCount } from '../renderer';
 
 export const [debugInfo, setDebugInfo] = createSignal<string>();
 export const [lineLyrics, setLineLyrics] = createSignal<LineLyrics[]>([]);
 export const [currentTime, setCurrentTime] = createSignal<number>(-1);
 
 export const LyricsContainer = () => {
-  // const [error, setError] = createSignal('');
-
-  const source = createMemo(() => providers[providerIdx()]);
-
-  const isLoading = createMemo(
-    () => searchResults().data[source().name].state === 'fetching'
-  );
-  const isError = createMemo(
-    () => searchResults().data[source().name].state === 'error'
+  // @ts-expect-error silly typescript
+  const error = createMemo(() =>
+    isError() ? lyricsSourceState().error : null
   );
 
-  const result = createMemo(() => searchResults().data[source().name]);
-  const lyrics = createMemo(() => {
-    const res = result();
-    if (res.state === 'done') return res.data;
-    return null;
-  });
-
-  // const fetch = () => {
-  //   // if (isFetching()) return;
-  //   setError('');
-  //
-  //   try {
-  //     const info = getSongInfo();
-  //     fetchLyrics(info);
-  //   } catch (error) {
-  //     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  //     setError(`${error}`);
-  //   }
-  // };
+  // @ts-expect-error silly typescript
+  const lyrics = createMemo(() => (isDone() ? lyricsSourceState().data : null));
 
   return (
     <div class="lyric-container">
+      <span style={{ display: 'none' }}>{renderCount()}</span>
       <Switch>
         <Match when={isLoading()}>
           <div style="margin-bottom: 8px;">
@@ -76,6 +55,7 @@ export const LyricsContainer = () => {
                 {
                   text: t('plugins.synced-lyrics.errors.fetch'),
                 },
+                { text: error() },
               ],
             }}
           />
@@ -151,7 +131,7 @@ export const LyricsContainer = () => {
       <yt-formatted-string
         class="ytmusic-description-shelf-renderer"
         text={{
-          runs: [{ text: '' }, { text: `Source: ${source().name}` }],
+          runs: [{ text: '' }, { text: `Source: ${lyricsSource().name}` }],
         }}
       />
     </div>
