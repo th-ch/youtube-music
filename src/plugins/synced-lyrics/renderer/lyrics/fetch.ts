41371486affb63435b80fa218d78b64334626bb3
Reycko <78082869+Reycko@users.noreply.github.com>
2024-09-23 17:48:10 +0200

    fix(synced-lyrics): fix miscalculation for lyrics' durations
    This fixes the lyrics' durations being incorrect due to unparsed lyrics offseting every new lyrics' indexes by 1.

--- src/plugins/synced-lyrics/renderer/lyrics/fetch.ts
+++ src/plugins/synced-lyrics/renderer/lyrics/fetch.ts
@@ -178,10 +178,12 @@ export const getLyricsList = async (
   // Add a blank line at the beginning
   raw.unshift('[0:0.0] ');
 
-  const syncedLyricList = raw.reduce<LineLyrics[]>((acc, line, index) => {
-    const syncedLine = extractTimeAndText(line, index);
+  let i = 0;
+  const syncedLyricList = raw.reduce<LineLyrics[]>((acc, line) => {
+    const syncedLine = extractTimeAndText(line, i);
     if (syncedLine) {
       acc.push(syncedLine);
+      i++;
     }
 
     return acc;
