e7f213c4dc1ddc09556a857b0a29947cedbb5776
JellyBrick <shlee1503@naver.com>
2024-10-13 18:22:23 +0900

    chore(eslint): apply eslint-plugin-prettier
    - Close #2438

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,67 +1,71 @@
 //@ts-check
 
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import stylistic from "@stylistic/eslint-plugin-js"
-import * as importPlugin from "eslint-plugin-import"
+import prettier from 'eslint-plugin-prettier/recommended';
+import stylistic from '@stylistic/eslint-plugin-js';
+import tsEslint from 'typescript-eslint';
 
-export default tseslint.config(
+import * as importPlugin from 'eslint-plugin-import';
+
+export default tsEslint.config(
   eslint.configs.recommended,
-  tseslint.configs.eslintRecommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  { ignores: ["dist", "node_modules", "*.config.*js"] },
+  tsEslint.configs.eslintRecommended,
+  ...tsEslint.configs.recommendedTypeChecked,
+  prettier,
+  { ignores: ['dist', 'node_modules', '*.config.*js'] },
   {
     plugins: {
       stylistic,
       importPlugin
     },
     languageOptions: {
-      parser: tseslint.parser,
+      parser: tsEslint.parser,
       parserOptions: {
         project: true,
-        sourceType: "module",
-        ecmaVersion: "latest"
+        sourceType: 'module',
+        ecmaVersion: 'latest'
       }
     },
     rules: {
-      "stylistic/arrow-parens": ["error", "always"],
-      "stylistic/object-curly-spacing": ["error", "always"],
-      "@typescript-eslint/no-floating-promises": "off",
-      "@typescript-eslint/no-misused-promises": ["off", { checksVoidReturn: false }],
+      'stylistic/arrow-parens': ['error', 'always'],
+      'stylistic/object-curly-spacing': ['error', 'always'],
+      'prettier/prettier': ['error', { singleQuote: true, semi: true, tabWidth: 2, trailingComma: 'all', quoteProps: 'preserve' }],
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-misused-promises': ['off', { checksVoidReturn: false }],
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "importPlugin/first": "error",
-      "importPlugin/newline-after-import": "off",
-      "importPlugin/no-default-export": "off",
-      "importPlugin/no-duplicates": "error",
-      "importPlugin/no-unresolved": ["error", { ignore: ['^virtual:', '\\?inline$', '\\?raw$', '\\?asset&asarUnpack'] }],
-      "importPlugin/order": ["error", {
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'importPlugin/first': 'error',
+      'importPlugin/newline-after-import': 'off',
+      'importPlugin/no-default-export': 'off',
+      'importPlugin/no-duplicates': 'error',
+      'importPlugin/no-unresolved': ['error', { ignore: ['^virtual:', '\\?inline$', '\\?raw$', '\\?asset&asarUnpack'] }],
+      'importPlugin/order': ['error', {
         'groups': ['builtin', 'external', ['internal', 'index', 'sibling'], 'parent', 'type'],
         'newlines-between': 'always-and-inside-groups',
         'alphabetize': { order: 'ignore', caseInsensitive: false }
       }],
-      "importPlugin/prefer-default-export": "off",
-      "camelcase": ["error", { properties: "never" }],
-      "class-methods-use-this": "off",
-      "stylistic/lines-around-comment": ["error", {
+      'importPlugin/prefer-default-export': 'off',
+      'camelcase': ['error', { properties: 'never' }],
+      'class-methods-use-this': 'off',
+      'stylistic/lines-around-comment': ['error', {
         beforeBlockComment: false,
         afterBlockComment: false,
         beforeLineComment: false,
         afterLineComment: false,
       }],
-      "stylistic/max-len": "off",
-      "stylistic/no-mixed-operators": "error",
-      "stylistic/no-multi-spaces": ["error", { ignoreEOLComments: true }],
-      "stylistic/no-tabs": "error",
-      "no-void": "error",
-      "no-empty": "off",
-      "prefer-promise-reject-errors": "off",
-      "stylistic/quotes": ["error", "single", {
+      'stylistic/max-len': 'off',
+      'stylistic/no-mixed-operators': 'error',
+      'stylistic/no-multi-spaces': ['error', { ignoreEOLComments: true }],
+      'stylistic/no-tabs': 'error',
+      'no-void': 'error',
+      'no-empty': 'off',
+      'prefer-promise-reject-errors': 'off',
+      'stylistic/quotes': ['error', 'single', {
         avoidEscape: true,
         allowTemplateLiterals: false,
       }],
-      "stylistic/quote-props": ["error", "consistent"],
-      "stylistic/semi": ["error", "always"],
+      'stylistic/quote-props': ['error', 'consistent'],
+      'stylistic/semi': ['error', 'always'],
     },
     settings: {
       'import/parsers': {
