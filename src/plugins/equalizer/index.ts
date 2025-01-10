4326c6fcd489994e857bcc506543891f207431c7
Oleh <olehivaniuk.net@gmail.com>
2025-01-10 13:14:56 +0200

    merge with old equalizer plugin

--- src/plugins/equalizer/index.ts
+++ src/plugins/equalizer/index.ts
@@ -1,16 +1,48 @@
 import { createPlugin } from '@/utils';
 import { t } from '@/i18n';
-import { MenuContext } from '@/types/contexts';
-import { MenuTemplate } from '@/menu';
-import { defaultPresets, presetConfigs, Preset, FilterConfig } from './presets';
+import { Preset, defaultCustomPresets, defaultPresets } from './presets';
 
-export type EqualizerPluginConfig = {
-  enabled: boolean;
-  filters: FilterConfig[];
-  presets: { [preset in Preset]: boolean };
-};
+import appConfig from '../../config';
 
-let appliedFilters: BiquadFilterNode[] = [];
+let filters: BiquadFilterNode[] = [];
+
+let storedAudioSource: AudioNode;
+let storedAudioContext: AudioContext;
+
+function clearFilters() {
+  filters.forEach((filter) => filter.disconnect());
+  filters = [];
+  storedAudioSource.disconnect();
+  storedAudioSource.connect(storedAudioContext.destination);
+}
+
+function createFilters(
+  preset: Preset,
+  audioContext: AudioContext,
+): BiquadFilterNode[] {
+  const filters = preset.filters.map((band) => {
+    const filter = audioContext.createBiquadFilter();
+    filter.type = band.type || ('peaking' as BiquadFilterType);
+    filter.frequency.value = band.freq || 0;
+    filter.Q.value = band.Q || 1;
+    filter.gain.value = band.gain || 0;
+    return filter;
+  });
+  return filters;
+}
+
+function connectFilters(
+  filters: BiquadFilterNode[],
+  audioSource: AudioNode,
+  audioContext: AudioContext,
+) {
+  let currentNode: AudioNode = audioSource;
+  for (const filter of filters) {
+    currentNode.connect(filter);
+    currentNode = filter;
+  }
+  currentNode.connect(audioContext.destination);
+}
 
 export default createPlugin({
   name: () => t('plugins.equalizer.name'),
@@ -19,63 +51,110 @@ export default createPlugin({
   addedVersion: '3.7.X',
   config: {
     enabled: false,
-    filters: [],
-    presets: { 'bass-booster': false },
-  } as EqualizerPluginConfig,
-  menu: async ({
-    getConfig,
-    setConfig,
-  }: MenuContext<EqualizerPluginConfig>): Promise<MenuTemplate> => {
+    selectedPreset: 'flat', // Default preset
+    customPresets: [] as Preset[],
+  },
+  menu: async ({ getConfig, setConfig }) => {
     const config = await getConfig();
+
+    const allPresetsD = config.customPresets.concat(defaultPresets);
+
+    const checkedPreset = allPresetsD.some(
+      (preset) => preset.name === config.selectedPreset,
+    )
+      ? config.selectedPreset
+      : 'flat';
 
     return [
       {
         label: t('plugins.equalizer.menu.presets.label'),
-        type: 'submenu',
-        submenu: defaultPresets.map((preset) => ({
-          label: t(`plugins.equalizer.menu.presets.list.${preset}`),
+        submenu: allPresetsD.map((preset) => ({
+          restartNeeded: true,
+          label: config.customPresets.includes(preset)
+            ? preset.name
+            : t(`plugins.equalizer.menu.presets.list.${preset.name}`),
           type: 'radio',
-          checked: config.presets[preset],
+          checked: checkedPreset === preset.name,
           click() {
-            setConfig({
-              presets: { ...config.presets, [preset]: !config.presets[preset] },
-            });
+            setConfig({ selectedPreset: preset.name });
           },
         })),
+      },
+      {
+        type: 'separator',
+      },
+      {
+        label: t('plugins.equalizer.menu.reset-custom-presets'),
+        toolTip: t('plugins.equalizer.menu.reset-custom-presets-tooltip'),
+        click() {
+          setConfig({ customPresets: defaultCustomPresets });
+        },
+      },
+      {
+        label: t('plugins.equalizer.menu.edit-config'),
+        toolTip: t('plugins.equalizer.menu.edit-config-tooltip'),
+        click() {
+          appConfig.edit();
+        },
       },
     ];
   },
   renderer: {
-    async start({ getConfig }) {
-      const config = await getConfig();
+    async start(context) {
+      const config = await context.getConfig();
 
-      document.addEventListener(
-        'ytmd:audio-can-play',
-        ({ detail: { audioSource, audioContext } }) => {
-          const filtersToApply = config.filters.concat(
-            defaultPresets
-              .filter((preset) => config.presets[preset])
-              .map((preset) => presetConfigs[preset]),
-          );
-          filtersToApply.forEach((filter) => {
-            const biquadFilter = audioContext.createBiquadFilter();
-            biquadFilter.type = filter.type;
-            biquadFilter.frequency.value = filter.frequency; // filter frequency in Hz
-            biquadFilter.Q.value = filter.Q;
-            biquadFilter.gain.value = filter.gain; // filter gain in dB
+      const allPresets = config.customPresets.concat(defaultPresets);
 
-            audioSource.connect(biquadFilter);
-            biquadFilter.connect(audioContext.destination);
+      const specifiedPreset =
+        allPresets.find((preset) => preset.name === config.selectedPreset) ||
+        allPresets.find((preset) => preset.name === 'flat');
 
-            appliedFilters.push(biquadFilter);
-          });
-        },
-        { once: true, passive: true },
-      );
+      if (!specifiedPreset) {
+        return;
+      }
+
+      if (!storedAudioSource || !storedAudioContext) {
+        document.addEventListener(
+          'ytmd:audio-can-play',
+          ({ detail: { audioSource, audioContext } }) => {
+            // Store audioSource and audioContext
+            storedAudioSource = audioSource;
+            storedAudioContext = audioContext;
+
+            filters = createFilters(specifiedPreset, audioContext);
+            audioSource.disconnect();
+            connectFilters(filters, audioSource, audioContext);
+          },
+          { once: true, passive: true },
+        );
+      } else {
+        filters = createFilters(specifiedPreset, storedAudioContext);
+        storedAudioSource.disconnect();
+        connectFilters(filters, storedAudioSource, storedAudioContext);
+      }
+    },
+    onConfigChange(newConfig) {
+      if (!storedAudioSource || !storedAudioContext) {
+        return;
+      }
+
+      clearFilters();
+
+      const allPresets = newConfig.customPresets.concat(defaultPresets);
+      const specifiedPreset =
+        allPresets.find((preset) => preset.name === newConfig.selectedPreset) ||
+        allPresets.find((preset) => preset.name === 'flat');
+
+      if (!specifiedPreset) {
+        return;
+      }
+
+      filters = createFilters(specifiedPreset, storedAudioContext);
+      storedAudioSource.disconnect();
+      connectFilters(filters, storedAudioSource, storedAudioContext);
     },
     stop() {
-      appliedFilters.forEach((filter) => filter.disconnect());
-      appliedFilters = [];
+      clearFilters();
     },
   },
 });
