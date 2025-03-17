90400398769d7f4ae184a36d1a26ecda1a03cece
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2025-03-17 22:01:03 +0800

    feat(plugin): add unobtrusive player plugin

---
+++ src/plugins/unobtrusive-player/index.ts
@@ -0,0 +1,71 @@
+import style from './style.css?inline';
+
+import { createPlugin } from '@/utils';
+import { t } from '@/i18n';
+
+export default createPlugin({
+  name: () => t('plugins.unobtrusive-player.name'),
+  description: () => t('plugins.unobtrusive-player.description'),
+  restartNeeded: false,
+  config: {
+    enabled: false,
+  },
+  stylesheets: [style],
+  renderer: {
+    start: () => {
+      document.body.classList.add('unobtrusive-player');
+
+      document.addEventListener('click', (e) => {
+        if (!(e.target instanceof HTMLElement)) return;
+
+        if (
+          e.target.closest('ytmusic-play-button-renderer') &&
+          !e.target.closest('ytmusic-player-page')
+        ) {
+          document.body.classList.add('unobtrusive-player--did-play');
+        }
+
+        if (
+          e.target.closest('ytmusic-player-bar') &&
+          !document.body.classList.contains('unobtrusive-player--auto-closing')
+        ) {
+          document.body.classList.remove('unobtrusive-player--did-play');
+        }
+      });
+    },
+    onPlayerApiReady: () => {
+      // Close player page when video changes while
+      // `unobtrusive-player--did-play` className is present.
+      document.addEventListener('videodatachange', () => {
+        const isPlayerPageOpen =
+          document
+            .querySelector('ytmusic-app-layout')
+            ?.attributes.getNamedItem('player-ui-state')?.value ===
+          'PLAYER_PAGE_OPEN';
+
+        if (
+          document.body.classList.contains('unobtrusive-player--did-play') &&
+          isPlayerPageOpen
+        ) {
+          document.body.classList.add('unobtrusive-player--auto-closing');
+
+          document
+            .querySelector<HTMLButtonElement>('.toggle-player-page-button')
+            ?.click();
+
+          // prevent animation flickering
+          setTimeout(() => {
+            document.body.classList.remove('unobtrusive-player--auto-closing');
+          }, 500);
+        }
+      });
+    },
+    stop: () => {
+      [
+        'unobtrusive-player',
+        'unobtrusive-player--did-play',
+        'unobtrusive-player--auto-closing',
+      ].forEach((className) => document.body.classList.remove(className));
+    },
+  },
+});
