21bd0c69a4015c144edb80a45d4f5d3c30ec0b41
ArjixWasTaken <me@arjix.dev>
2025-03-26 03:40:51 +0200

    improve plugins tab

--- src/plugins/settings-ui/components/settings/Plugins.tsx
+++ src/plugins/settings-ui/components/settings/Plugins.tsx
@@ -1,4 +1,4 @@
-import { createSignal, For, Show } from 'solid-js';
+import { createEffect, createSignal, For, Show } from 'solid-js';
 import { allPlugins } from 'virtual:plugins';
 import { plugins } from '../../renderer';
 // import config from '@/config';
@@ -57,24 +57,81 @@ const PluginCard = (props: PluginCardProps) => {
   );
 };
 
+// prettier-ignore
 export default () => {
   const availablePlugins = Object.keys(allPlugins);
 
+  const [query, setQuery] = createSignal('');
+  const [filter, setFilter] = createSignal<'all' | 'enabled' | 'disabled'>('all');
+  const [filteredPlugins, setFilteredPlugins] = createSignal<string[]>(availablePlugins);
+
+  createEffect(() => {
+    if (filter() === 'all') {
+      setFilteredPlugins(Array.from(availablePlugins));
+      return;
+    }
+
+    (async () => {
+      const filtered: string[] = [];
+
+      for (const plugin of availablePlugins) {
+        const enabled = await plugins.isEnabled(plugin);
+
+        if (filter() === 'enabled' && enabled) {
+          filtered.push(plugin);
+        } else if (filter() === 'disabled' && !enabled) {
+          filtered.push(plugin);
+        }
+      }
+
+      // TODO: Add search functionality
+
+      setFilteredPlugins(filtered);
+    })();
+  });
+
   return (
-    <div class="ytmd-sui-settingsContent ytmd-sui-pluginsSection">
-      <For each={availablePlugins}>
-        {(pluginId) => {
-          const plugin = allPlugins[pluginId];
-          return (
-            <PluginCard
-              id={pluginId}
-              label={plugin.name()}
-              description={plugin.description?.() ?? 'N/A'}
-              authors={plugin.authors}
-            />
-          );
-        }}
-      </For>
+    <div class="ytmd-sui-settingsContent">
+      <div class="ytmd-sui-pluginsFilters">
+        <div class="ytmd-sui-pluginsSearch">
+          <input
+            type="text"
+            class="ytmd-sui-input"
+            onInput={(e) => setQuery(e.currentTarget.value)}
+            placeholder="Search plugins..."
+          />
+        </div>
+        <div class="ytmd-sui-settingText">
+          <div class="ytmd-sui-settingLabel">
+            <span class="ytmd-sui-settingTitle">Show</span>
+          </div>
+          <select
+            class="ytmd-sui-select"
+            tabIndex={1}
+            value="all"
+            onChange={(e) => setFilter(e.target.value as any)}
+          >
+            <option value="all">All</option>
+            <option value="enabled">Enabled</option>
+            <option value="disabled">Disabled</option>
+          </select>
+        </div>
+      </div>
+      <div class="ytmd-sui-pluginsSection">
+        <For each={filteredPlugins()}>
+          {(pluginId) => {
+            const plugin = allPlugins[pluginId];
+            return (
+              <PluginCard
+                id={pluginId}
+                label={plugin.name()}
+                description={plugin.description?.() ?? 'N/A'}
+                authors={plugin.authors}
+              />
+            );
+          }}
+        </For>
+      </div>
     </div>
   );
 };
