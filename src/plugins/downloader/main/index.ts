3f442a97c5d9a2946a37272d36cf8af9dfbdc375
JellyBrick <shlee1503@naver.com>
2025-07-10 16:40:33 +0900

    feat: remove lyrics-genius

--- src/plugins/downloader/main/index.ts
+++ src/plugins/downloader/main/index.ts
@@ -19,8 +19,6 @@ import {
   sendFeedback as sendFeedback_,
   setBadge,
 } from './utils';
-import { fetchFromGenius } from '@/plugins/lyrics-genius/main';
-import { isEnabled } from '@/config/plugins';
 import registerCallback, {
   cleanupName,
   getImage,
@@ -596,16 +594,6 @@ async function writeID3(
         description: 'thumbnail',
         imageBuffer: coverBuffer,
       };
-    }
-
-    if (isEnabled('lyrics-genius')) {
-      const lyrics = await fetchFromGenius(metadata);
-      if (lyrics) {
-        tags.unsynchronisedLyrics = {
-          language: '',
-          text: lyrics,
-        };
-      }
     }
 
     if (metadata.trackId) {
