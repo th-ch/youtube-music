b4179b9bff2a012a5d1459faf38419056ab161f0
oguz <oguz@oguz-MacBook-Pro.local>
2025-07-17 19:44:54 +0300

    17.07

--- src/plugins/find-in-playlist/renderer.ts
+++
@@ -1,7 +0,0 @@
-import { getSongInfo } from '@/providers/song-info-front';
-import { SingleColumnMusicWatchNextResultsRenderer } from '@/types/player-api-events';
-
-export const onPlayerApiReady = () => {};
-export const onUnloadPlaylist = () => {
-  console.log('');
-};
