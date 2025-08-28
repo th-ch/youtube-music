215d46c9ad37d6187fa6cbce597c8f7b1fcf31ba
Lucas <68608785+kev2block@users.noreply.github.com>
2025-08-28 21:34:09 +0200

    Add files via upload

--- src/plugins/src/index.ts
+++ src/plugins/src/index.ts
@@ -1,1 +1,29 @@
+import { createPlugin } from '@/utils';
 
+import { menu } from './menu';
+import { renderer } from './renderer';
+
+import type { FontCustomizerConfig } from './types';
+
+const defaultConfig: FontCustomizerConfig = {
+  enabled: true,
+  mode: 'simple',
+  globalFont: 'System Default',
+  primaryFont: 'System Default',
+  headerFont: 'System Default',
+  titleFont: 'System Default',
+  artistFont: 'System Default',
+  lyricsFont: 'System Default',
+  menuFont: 'System Default',
+  customFonts: [],
+};
+
+export default createPlugin({
+  name: () => 'Font Customizer',
+  description: () =>
+    'Change fonts globally or specifically for song title, artist, lyrics, and navigation menu. Uses Google Fonts.',
+  restartNeeded: false,
+  config: defaultConfig,
+  menu,
+  renderer,
+});
