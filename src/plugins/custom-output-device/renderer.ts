72ded6356fb925ed6a670e4d122a694ec3dc00e4
Benjas333 <seston30@gmail.com>
2025-09-05 10:32:48 -0600

    Applied lint Removed console logs

--- src/plugins/custom-output-device/renderer.ts
+++ src/plugins/custom-output-device/renderer.ts
@@ -1,59 +1,69 @@
-import { YoutubePlayer } from "@/types/youtube-player";
-import { CustomOutputPluginConfig } from ".";
-import { RendererContext } from "@/types/contexts";
-import { createRenderer } from "@/utils";
+import { type YoutubePlayer } from '@/types/youtube-player';
+import { type CustomOutputPluginConfig } from '.';
+import { type RendererContext } from '@/types/contexts';
+import { createRenderer } from '@/utils';
 
-const updateDeviceList = async (context: RendererContext<CustomOutputPluginConfig>) => {
+const updateDeviceList = async (
+  context: RendererContext<CustomOutputPluginConfig>,
+) => {
   const newDevices: Record<string, string> = {};
-  const devices = await navigator.mediaDevices.enumerateDevices().then(devices => devices.filter(device => device.kind === 'audiooutput'));
+  const devices = await navigator.mediaDevices
+    .enumerateDevices()
+    .then((devices) =>
+      devices.filter((device) => device.kind === 'audiooutput'),
+    );
   for (const device of devices) {
     newDevices[device.deviceId] = device.label;
   }
   const options = await context.getConfig();
   options.devices = newDevices;
   context.setConfig(options);
-}
+};
 
 const updateSinkId = async (audioContext?: AudioContext, sinkId?: string) => {
   if (!audioContext || !sinkId) return;
+  if (!('setSinkId' in audioContext)) return;
+  if (typeof audioContext.setSinkId !== 'function') return;
 
-  if (!('setSinkId' in audioContext)) {
-    console.log('setSinkId not in context')
-    return;
-  }
-  if (typeof audioContext.setSinkId !== 'function') {
-    console.log('setSinkId not a function')
-    return;
-  }
-  await audioContext.setSinkId(sinkId)
-}
+  await audioContext.setSinkId(sinkId);
+};
 
-export const renderer = createRenderer<{
-  options?: CustomOutputPluginConfig;
-  audioContext?: AudioContext;
-  audioCanPlayHandler: (event: CustomEvent<Compressor>) => Promise<void>;
-}, CustomOutputPluginConfig>({
+export const renderer = createRenderer<
+  {
+    options?: CustomOutputPluginConfig;
+    audioContext?: AudioContext;
+    audioCanPlayHandler: (event: CustomEvent<Compressor>) => Promise<void>;
+  },
+  CustomOutputPluginConfig
+>({
   async audioCanPlayHandler({ detail: { audioContext } }) {
-    this.audioContext = audioContext
+    this.audioContext = audioContext;
     await updateSinkId(audioContext, this.options!.output);
   },
 
   async onPlayerApiReady(_: YoutubePlayer, context) {
     this.options = await context.getConfig();
     await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-    navigator.mediaDevices.ondevicechange = async () => await updateDeviceList(context);
+    navigator.mediaDevices.ondevicechange = async () =>
+      await updateDeviceList(context);
 
-    document.addEventListener('ytmd:audio-can-play', this.audioCanPlayHandler, { once: true, passive: true });
+    document.addEventListener('ytmd:audio-can-play', this.audioCanPlayHandler, {
+      once: true,
+      passive: true,
+    });
     await updateDeviceList(context);
   },
 
   stop() {
-    document.removeEventListener('ytmd:audio-can-play', this.audioCanPlayHandler);
+    document.removeEventListener(
+      'ytmd:audio-can-play',
+      this.audioCanPlayHandler,
+    );
     navigator.mediaDevices.ondevicechange = null;
   },
 
   async onConfigChange(config) {
     this.options = config;
     await updateSinkId(this.audioContext, config.output);
-  }
+  },
 });
